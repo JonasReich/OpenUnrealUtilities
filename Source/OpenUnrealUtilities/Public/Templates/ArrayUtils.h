7b51c4c3738628f46f0acec34e4eab51893ed50d
Jonas Reich <mail@jonasreich.de>
2020-05-22 21:24:55 +0200

    Added array utils

---
+++ Source/OpenUnrealUtilities/Public/Templates/ArrayUtils.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2020 Jonas Reich
+
+#pragma once
+
+#include "Containers/Array.h"
+
+class OPENUNREALUTILITIES_API FArrayUtils
+{
+public:
+	/** Set all existing elements in an array to the same value */
+	template<typename ElementType, typename AllocatorType>
+	static void SetAllTo(TArray<ElementType, AllocatorType>& Array, const ElementType& Value)
+	{
+		for(int32 i = 0; i < Array.Num(); i++)
+		{
+			Array[i] = Value;
+		}
+	}
+
+	/**
+	 * Change an array to the specified length (same as the SetNum() member function)
+	 * and assign all elements to the same value.
+	 */
+	template<typename ElementType, typename AllocatorType>
+	static void SetNumTo(TArray<ElementType, AllocatorType>& Array, int32 Num, const ElementType& Value)
+	{
+		Array.SetNum(Num);
+		SetAllTo(Array, Value);
+	}
+};
