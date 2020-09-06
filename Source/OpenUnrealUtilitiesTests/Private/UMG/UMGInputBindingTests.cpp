36d4229db87313897323557687b1e99a56596631
Jonas Reich <mail@jonasreich.de>
2020-09-06 05:12:49 +0200

    Added spec for UMGInputBindings

--- Source/OpenUnrealUtilitiesTests/Private/UMG/UMGInputBindingTests.cpp
+++ Source/OpenUnrealUtilitiesTests/Private/UMG/UMGInputBindingTests.cpp
@@ -10,53 +10,326 @@
 #include "OpenUnrealUtilitiesMacros.h"
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/PlayerInput.h"
+#include "Templates/BoolRange.h"
 
-#define OUU_TEST_CATEGORY OpenUnrealUtilities
-#define OUU_TEST_TYPE UMGInputActionBinding
-
-//////////////////////////////////////////////////////////////////////////
-
-#define ASDF EAutomationTestFlags::ClientContext | EAutomationTestFlags::ProductFilter
-OUU_IMPLEMENT_SIMPLE_AUTOMATION_TEST(BindAction, DEFAULT_OUU_TEST_FLAGS)
+FKeyEvent SimulateKeyEvent(UPlayerInput* PlayerInput, UUMGInputBindingTestWidget* Widget, FKey Key, EInputEvent Event)
 {
-	// Arrange
-	FScopedAutomationWorld TestWorld;
-	TestWorld.BeginPlay();
-	if (!TestWorld.InitiailizeGame())
-		return false;
-	UWorld* World = TestWorld.World;
-	
-	static FName TestActionName = "TestActionName";
-	TestWorld.PlayerController->PlayerInput->AddActionMapping(FInputActionKeyMapping(TestActionName, EKeys::SpaceBar));
-	
- 	UUMGInputBindingTestWidget* Widget = Cast<UUMGInputBindingTestWidget>(UUserWidget::CreateWidgetInstance(*TestWorld.PlayerController, UUMGInputBindingTestWidget::StaticClass(), NAME_None));
-	Widget->AddToPlayerScreen();
-	UUMGInputActionBindingStack* Stack = UUMGInputActionBindingStack::CreateUMGInputActionBindingStack(Widget);
-
-	FUMGInputAction Action;
-	Action.ActionName = TestActionName;
-	Action.ReactEvent = EUMGInputActionKeyEvent::KeyDown;
-	Action.ConsumeEvent = EUMGInputActionKeyEvent::KeyDown;
-	Stack->BindAction(Action, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEvent));
-	
-	// Only the FKey should matter
-	FKeyEvent KeyEvent{ EKeys::SpaceBar, FModifierKeysState(), 0, false, 0, 0 };
-	
-	// Act
-	TestWorld.PlayerController->PlayerInput->InputKey(EKeys::SpaceBar, EInputEvent::IE_Pressed, 0.f, false);
+	FKeyEvent KeyEvent{ Key, FModifierKeysState(), 0, false, 0, 0 };
+	PlayerInput->InputKey(Key, Event, 0.f, false);
 	TArray<UInputComponent*> InputComponents;
-	TestWorld.PlayerController->PlayerInput->ProcessInputStack(InputComponents, 0.f, false);
-	FEventReply Reply = Stack->ProcessOnKeyDown(FGeometry(), KeyEvent);
-
-	// Assert
-	TestTrue("Event was handled", Reply.NativeReply.IsEventHandled());
-
-	return true;
+	PlayerInput->ProcessInputStack(InputComponents, 0.f, false);
+	switch (Event)
+	{
+	case EInputEvent::IE_Pressed:
+	{
+		Widget->NativeOnKeyDown(FGeometry(), KeyEvent);
+		break;
+	}
+	case IE_Released:
+	{
+		Widget->NativeOnKeyUp(FGeometry(), KeyEvent);
+		break;
+	}
+	}
+	return KeyEvent;
 }
 
-//////////////////////////////////////////////////////////////////////////
+BEGIN_DEFINE_SPEC(FUMGInputActionBindingSpec, "OpenUnrealUtilities.UMGInputActionBinding", DEFAULT_OUU_TEST_FLAGS)
+FAutomationTestWorld TestWorld;
+UPlayerInput* PlayerInput;
+UUMGInputBindingTestWidget* Widget;
+UUMGInputActionBindingStack* Stack;
+END_DEFINE_SPEC(FUMGInputActionBindingSpec)
+void FUMGInputActionBindingSpec::Define()
+{
+	BeforeEach([this]()
+	{
+		TestWorld.CreateWorld();
+		TestWorld.InitiailizeGame();
+		PlayerInput = TestWorld.PlayerController->PlayerInput;
 
-#undef OUU_TEST_CATEGORY
-#undef OUU_TEST_TYPE
+		Widget = Cast<UUMGInputBindingTestWidget>(UUserWidget::CreateWidgetInstance(*TestWorld.PlayerController, UUMGInputBindingTestWidget::StaticClass(), NAME_None));
+		Widget->AddToPlayerScreen();
+		Stack = UUMGInputActionBindingStack::CreateUMGInputActionBindingStack(Widget);
+		Widget->Stack = Stack;
+	});
+
+	Describe("CreateUMGInputActionBindingStack", [this]()
+	{
+		It("should create a valid stack with the correct player input set", [this]()
+		{
+			TestTrue("valid stack", IsValid(Stack));
+			TestEqual("OwningPlayerInput", Stack->GetOwningPlayerInput(), PlayerInput);
+		});
+	});
+
+	Describe("SetOwningPlayerInput", [this]()
+	{
+		It("should change the owning player input", [this]()
+		{
+			Stack->SetOwningPlayerInput(nullptr);
+			TestNull("OwningPlayer after setting nullptr", Stack->GetOwningPlayerInput());
+			Stack->SetOwningPlayerInput(PlayerInput);
+			TestTrue("OwningPlayer is same", Stack->GetOwningPlayerInput() == PlayerInput);
+		});
+	});
+
+	Describe("Binding a simple key down event", [this]()
+	{
+		BeforeEach([this]() 
+		{
+			const FName TestActionName = "TestAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(TestActionName, EKeys::One));
+			FUMGInputAction ActionOne{ TestActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+			Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+		});
+		
+		It("should result in a single bindings being present with the target object", [this]()
+		{
+			TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 1);
+		});
+
+		It("should result in a delegate call and event consumption if the same key is pressed", [this]()
+		{
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::One, IE_Pressed);
+			TestEqual("Number of delegate calls", Widget->HandleInputActionEventOne_NumCalls, 1);
+			TestTrue("Input event was consumed", Widget->bLastNativeOnKeyDownWasHandled);
+		});
+		
+		It("should result in NO delegate call and event consumption if the same key is released", [this]()
+		{
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::One, IE_Released);
+			TestEqual("Number of delegate calls", Widget->HandleInputActionEventOne_NumCalls, 0);
+			TestFalse("Input event was consumed", Widget->bLastNativeOnKeyDownWasHandled);
+		});
+
+		It("should result in two delegate calls if the same key is pressed twice", [this]() 
+		{
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::One, IE_Pressed);
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::One, IE_Pressed);
+			TestEqual("Number of delegate calls", Widget->HandleInputActionEventOne_NumCalls, 2);
+		});
+	});
+
+	Describe("Binding two events with different keys", [this]()
+	{
+		BeforeEach([this]()
+		{
+			const FName FirstActionName = "FirstAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(FirstActionName, EKeys::One));
+			FUMGInputAction ActionOne{ FirstActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+			Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+			
+			const FName SecondActionName = "SecondAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(SecondActionName, EKeys::Two));
+			FUMGInputAction ActionTwo{ SecondActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+			Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+		});
+
+		It("should result in two bindings being present with the target object", [this]()
+		{
+			TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 2);
+		});
+
+		It("should trigger the first action if the matching key is pressed", [this]()
+		{
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::One, IE_Pressed);
+			TestEqual("Number of delegate calls (first action)", Widget->HandleInputActionEventOne_NumCalls, 1);
+			TestEqual("Number of delegate calls (second action)", Widget->HandleInputActionEventTwo_NumCalls, 0);
+		});
+
+		It("should trigger the second action if the matching key is pressed", [this]()
+		{
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::Two, IE_Pressed);
+			TestEqual("Number of delegate calls (first action)", Widget->HandleInputActionEventOne_NumCalls, 0);
+			TestEqual("Number of delegate calls (second action)", Widget->HandleInputActionEventTwo_NumCalls, 1);
+		});
+	});
+
+	Describe("Binding two events with the same key", [this]()
+	{
+		BeforeEach([this]()
+		{
+			const FName FirstActionName = "FirstAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(FirstActionName, EKeys::One));
+			FUMGInputAction ActionOne{ FirstActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+			Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+
+			const FName SecondActionName = "SecondAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(SecondActionName, EKeys::One));
+			FUMGInputAction ActionTwo{ SecondActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+			Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+		});
+
+		It("should trigger the first bound action if the matching key is pressed", [this]()
+		{
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::One, IE_Pressed);
+			TestEqual("Number of delegate calls (first action)", Widget->HandleInputActionEventOne_NumCalls, 1);
+			TestEqual("Number of delegate calls (second action)", Widget->HandleInputActionEventTwo_NumCalls, 0);
+		});
+	});
+
+	Describe("Marking an action as bIsOneshot", [this]()
+	{
+		BeforeEach([this]()
+		{
+			const FName FirstActionName = "FirstAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(FirstActionName, EKeys::One));
+			FUMGInputAction ActionOne{ FirstActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same, /*bIsOneshot*/ true };
+			Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+		});
+
+		It("should unbind the event after the first call", [this]()
+		{
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::One, IE_Pressed);
+			TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 0);
+			SimulateKeyEvent(PlayerInput, Widget, EKeys::One, IE_Pressed);
+			TestEqual("Number of delegate calls", Widget->HandleInputActionEventOne_NumCalls, 1);
+		});
+	});
+
+	Describe("RemoveSingleBinding", [this]()
+	{
+		It("should remove only the binding specified", [this]()
+		{
+			const FName FirstActionName = "FirstAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(FirstActionName, EKeys::One));
+			FUMGInputAction ActionOne{ FirstActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+			Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+
+			const FName SecondActionName = "SecondAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(SecondActionName, EKeys::One));
+			FUMGInputAction ActionTwo{ SecondActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+			Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+
+			Stack->RemoveSingleBinding(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+
+			TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 1);
+		});
+
+		It("should not remove a binding if the delegate doesn't match", [this]()
+		{
+			const FName FirstActionName = "FirstAction";
+			PlayerInput->AddActionMapping(FInputActionKeyMapping(FirstActionName, EKeys::One));
+			FUMGInputAction ActionOne{ FirstActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+			Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+			
+			Stack->RemoveSingleBinding(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+
+			TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 1);
+		});
+	});
+	
+	It("RemoveBindings should remove the bindings of the same type", [this]()
+	{
+		const FName FirstActionName = "FirstAction";
+		PlayerInput->AddActionMapping(FInputActionKeyMapping(FirstActionName, EKeys::One));
+		FUMGInputAction ActionOne{ FirstActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+		Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+
+		const FName SecondActionName = "SecondAction";
+		PlayerInput->AddActionMapping(FInputActionKeyMapping(SecondActionName, EKeys::One));
+		FUMGInputAction ActionTwo{ SecondActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+		Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+		Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventThree));
+
+		TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 3);
+		Stack->RemoveBindings(ActionTwo);
+		TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 1);
+	});
+
+	It("RemoveAllBindings should remove all bindings no matter the signature", [this]()
+	{
+		const FName FirstActionName = "FirstAction";
+		PlayerInput->AddActionMapping(FInputActionKeyMapping(FirstActionName, EKeys::One));
+		FUMGInputAction ActionOne{ FirstActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+		Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+
+		const FName SecondActionName = "SecondAction";
+		PlayerInput->AddActionMapping(FInputActionKeyMapping(SecondActionName, EKeys::One));
+		FUMGInputAction ActionTwo{ SecondActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+		Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+		Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventThree));
+
+		TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 3);
+		Stack->RemoveAllBindings();
+		TestEqual("Number of bindings", Stack->GetNumBindingsToObject(Widget), 0);
+	});
+
+	It("RemoveBindingByObject should remove only the bindings to the target object", [this]()
+	{
+		UUMGInputBindingTestWidget* SecondWidget = Cast<UUMGInputBindingTestWidget>(UUserWidget::CreateWidgetInstance(*TestWorld.PlayerController, UUMGInputBindingTestWidget::StaticClass(), NAME_None));
+
+		const FName FirstActionName = "FirstAction";
+		PlayerInput->AddActionMapping(FInputActionKeyMapping(FirstActionName, EKeys::One));
+		FUMGInputAction ActionOne{ FirstActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+		Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+		Stack->BindAction(ActionOne, CreateDynamic(FUMGInputActionDelegate, SecondWidget, &UUMGInputBindingTestWidget::HandleInputActionEventOne));
+
+		const FName SecondActionName = "SecondAction";
+		PlayerInput->AddActionMapping(FInputActionKeyMapping(SecondActionName, EKeys::One));
+		FUMGInputAction ActionTwo{ SecondActionName, EUMGInputActionKeyEvent::KeyDown, EUMGInputActionKeyEventConsumeMode::Same };
+		Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, Widget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+		Stack->BindAction(ActionTwo, CreateDynamic(FUMGInputActionDelegate, SecondWidget, &UUMGInputBindingTestWidget::HandleInputActionEventTwo));
+
+		TestEqual("Number of bindings (widget 1) before removal", Stack->GetNumBindingsToObject(Widget), 2);
+		TestEqual("Number of bindings (widget 2) before removal", Stack->GetNumBindingsToObject(SecondWidget), 2);
+		Stack->RemoveBindingsByObject(Widget);
+		TestEqual("Number of bindings (widget 1) after removal", Stack->GetNumBindingsToObject(Widget), 0);
+		TestEqual("Number of bindings (widget 2) after removal", Stack->GetNumBindingsToObject(SecondWidget), 2);
+	});
+
+	AfterEach([this]() 
+	{
+		TestWorld.DestroyWorld();
+	});
+}
 
 #endif
+
+void UUMGInputBindingTestWidget::HandleInputActionEventOne(EUMGInputActionKeyEvent SourceEvent)
+{
+	HandleInputActionEventOne_NumCalls++;
+	HandleInputActionEventOne_SourceEvent = SourceEvent;
+}
+
+void UUMGInputBindingTestWidget::HandleInputActionEventTwo(EUMGInputActionKeyEvent SourceEvent)
+{
+	HandleInputActionEventTwo_NumCalls++;
+	HandleInputActionEventTwo_SourceEvent = SourceEvent;
+}
+
+void UUMGInputBindingTestWidget::HandleInputActionEventThree(EUMGInputActionKeyEvent SourceEvent)
+{
+	// do nothing
+}
+
+FReply UUMGInputBindingTestWidget::NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent)
+{
+	if (IsValid(Stack))
+	{
+		auto Result = Stack->ProcessOnKeyDown(InGeometry, InKeyEvent);
+		if (Result.NativeReply.IsEventHandled())
+		{
+			bLastNativeOnKeyDownWasHandled = true;
+			return Result.NativeReply;
+		}
+	}
+	bLastNativeOnKeyDownWasHandled = false;
+	return Super::NativeOnKeyDown(InGeometry, InKeyEvent);
+}
+
+FReply UUMGInputBindingTestWidget::NativeOnKeyUp(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent)
+{
+	if (IsValid(Stack))
+	{
+		auto Result = Stack->ProcessOnKeyUp(InGeometry, InKeyEvent);
+		if (Result.NativeReply.IsEventHandled())
+		{
+			bLastNativeOnKeyUpWasHandled = true;
+			return Result.NativeReply;
+		}
+	}
+	bLastNativeOnKeyUpWasHandled = false;
+	return Super::NativeOnKeyUp(InGeometry, InKeyEvent);
+}
--- Source/OpenUnrealUtilitiesTests/Private/UMG/UMGInputBindingTests.h
+++ Source/OpenUnrealUtilitiesTests/Private/UMG/UMGInputBindingTests.h
@@ -4,22 +4,38 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "UMG/UMGInputBinding.h"
 #include "UMGInputBindingTests.generated.h"
+
+class UUMGInputActionBindingStack;
 
 UCLASS()
 class UUMGInputBindingTestWidget : public UUserWidget
 {
 	GENERATED_BODY()
 public:
+	UPROPERTY()
+	UUMGInputActionBindingStack* Stack;
+
+	bool bLastNativeOnKeyDownWasHandled = false;
+	bool bLastNativeOnKeyUpWasHandled = false;
+
+	int32 HandleInputActionEventOne_NumCalls = 0;
+	EUMGInputActionKeyEvent HandleInputActionEventOne_SourceEvent = EUMGInputActionKeyEvent::Any;
+
 	UFUNCTION()
-	void HandleInputActionEvent(EUMGInputActionKeyEvent SourceEvent)
-	{
+	void HandleInputActionEventOne(EUMGInputActionKeyEvent SourceEvent);
 
-	}
+	int32 HandleInputActionEventTwo_NumCalls = 0;
+	EUMGInputActionKeyEvent HandleInputActionEventTwo_SourceEvent = EUMGInputActionKeyEvent::Any;
 
 	UFUNCTION()
-	void HandleInputActionEventTwo(EUMGInputActionKeyEvent SourceEvent)
-	{
+	void HandleInputActionEventTwo(EUMGInputActionKeyEvent SourceEvent);
+	
+	// This one does nothing
+	UFUNCTION()
+	void HandleInputActionEventThree(EUMGInputActionKeyEvent SourceEvent);
 
-	}
+	FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
+	FReply NativeOnKeyUp(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
 };
