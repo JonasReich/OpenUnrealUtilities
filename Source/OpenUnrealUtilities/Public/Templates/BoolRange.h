745c6d2463050bc01b8d4e1464d54855524836c5
Jonas Reich <mail@jonasreich.de>
2020-09-06 05:05:54 +0200

    Added BoolRange

---
+++ Source/OpenUnrealUtilities/Public/Templates/BoolRange.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Jonas Reich
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * Utility function to be used in ranged based for loops for adding bool variants to code.
+ * Especially useful in automated testcase generation (e.g. test with feature toggled on and off).
+ */
+FORCEINLINE TArray<bool, TInlineAllocator<2>> BoolRange()
+{
+	return {false, true};
+}
---
+++ Source/OpenUnrealUtilitiesTests/Private/Templates/BoolRange.spec.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2020 Jonas Reich
+
+#include "OUUTests.h"
+
+#if WITH_AUTOMATION_WORKER
+
+#include "Templates/BoolRange.h"
+#include "Templates/ReverseIterator.h"
+
+BEGIN_DEFINE_SPEC(FBoolRangeSpec, "OpenUnrealUtilities.BoolRange", DEFAULT_OUU_TEST_FLAGS)
+END_DEFINE_SPEC(FBoolRangeSpec)
+void FBoolRangeSpec::Define()
+{
+	Describe("", [this]()
+	{
+		It("should be a single pair of {false, true}", [this]()
+		{
+			TArray<bool> Booleans;
+			for (bool Boolean : BoolRange())
+			{
+				Booleans.Add(Boolean);
+			}
+			TestEqual("Booleans", Booleans, { false, true });
+		});
+		It("should be a single pair of {true, false} when reversed", [this]() 
+		{
+			TArray<bool> Booleans;
+			for (bool Boolean : ReverseRange(BoolRange()))
+			{
+				Booleans.Add(Boolean);
+			}
+			TestEqual("Booleans", Booleans, { true, false });
+		});
+	});
+}
+
+#endif
