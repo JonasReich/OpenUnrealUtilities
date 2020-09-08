a4c9221f11f13b2275f91ff3ae5d75996b0761b3
Jonas Reich <mail@jonasreich.de>
2020-09-08 19:10:35 +0200

    Added OUUTestWidget next to OUUTestObject for UUserWidget tests

--- Source/OpenUnrealUtilitiesTests/Public/TestUtilities/OUUTestObject.h
+++ Source/OpenUnrealUtilitiesTests/Public/TestUtilities/OUUTestObject.h
@@ -3,11 +3,19 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
 #include "OUUTestObject.generated.h"
 
 /** Used in place of plain UObject for tests, because UObject itself is abstract */
 UCLASS(hidedropdown)
 class OPENUNREALUTILITIESTESTS_API UOUUTestObject : public UObject
+{
+	GENERATED_BODY()
+};
+
+/** Used in place of plain UUserWidget for tests, because UUserWidget itself is abstract */
+UCLASS(hidedropdown)
+class OPENUNREALUTILITIESTESTS_API UOUUTestWidget : public UUserWidget
 {
 	GENERATED_BODY()
 };
