fd1d0bb368f92eefba67853a22aca881a1e4b726
Jonas Reich <mail@jonasreich.de>
2020-09-08 19:11:29 +0200

    Added SPEC_TEST_* macros for simplified spec definitions

--- Source/OpenUnrealUtilitiesTests/Public/TestUtilities/OUUTestMacros.h
+++ Source/OpenUnrealUtilitiesTests/Public/TestUtilities/OUUTestMacros.h
@@ -22,70 +22,70 @@
  */
 #define DEFAULT_OUU_TEST_FLAGS EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter
 
- /**
-  * Use this macro in conjunction with a predefined OUU_TEST_CATEGORY and OUU_TEST_TYPE to declare a simple automation test.
-  * Used like this:
-  * -------------------------
-  * #define OUU_TEST_CATEGORY OpenUnrealUtilities.Foo
-  * #define OUU_TEST_TYPE Bar
-  * OUU_IMPLEMENT_SIMPLE_AUTOMATION_TEST(HelloWorld, DEFAULT_OUU_TEST_FLAGS)
-  * {
-  *     // test case implementation
-  * }
-  * #undef OUU_TEST_CATEGORY
-  * #undef OUU_TEST_TYPE
-  * -------------------------
-  * ...which declares a test class FBarTests_HelloWorld in the "OpenUnrealUtilities.Foo" category.
-  * For more thorough examples, refer to any of the tests declared in this module.
-  */
+/**
+ * Use this macro in conjunction with a predefined OUU_TEST_CATEGORY and OUU_TEST_TYPE to declare a simple automation test.
+ * Used like this:
+ * -------------------------
+ * #define OUU_TEST_CATEGORY OpenUnrealUtilities.Foo
+ * #define OUU_TEST_TYPE Bar
+ * OUU_IMPLEMENT_SIMPLE_AUTOMATION_TEST(HelloWorld, DEFAULT_OUU_TEST_FLAGS)
+ * {
+ *     // test case implementation
+ * }
+ * #undef OUU_TEST_CATEGORY
+ * #undef OUU_TEST_TYPE
+ * -------------------------
+ * ...which declares a test class FBarTests_HelloWorld in the "OpenUnrealUtilities.Foo" category.
+ * For more thorough examples, refer to any of the tests declared in this module.
+ */
 #define OUU_IMPLEMENT_SIMPLE_AUTOMATION_TEST(TestCase, TestFlags) \
 OUU_IMPLEMENT_SIMPLE_AUTOMATION_TEST_INNER(OUU_TEST_CLASS(TestCase), OUU_TESTCASE_PRETTY_NAME(TestCase), TestFlags)
 
-  /**
-   * Use this macro in conjunction with a predefined OUU_TEST_CATEGORY and OUU_TEST_TYPE to declare a complex automation test.
-   * In contrast to simple tests, complex automation tests are called on multiple test-cases based on strings.
-   * This utility allows declaring those test-cases in a simple list instead of manually implementing the GetTests() function of the test class.
-   * Used like this:
-   * -------------------------
-   * OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_BEGIN(PrintString, DEFAULT_OUU_TEST_FLAGS)
-   * OUU_COMPLEX_AUTOMATION_TESTCASE("Hello")
-   * OUU_COMPLEX_AUTOMATION_TESTCASE("World")
-   * OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_END(PrintString)
-   * {
-   *     // test case implementation
-   *     // use parameter (const FString& Parameters) to get one of
-   *     // the test-case strings "Hello" and "World"
-   * }
-   * -------------------------
-   */
+/**
+ * Use this macro in conjunction with a predefined OUU_TEST_CATEGORY and OUU_TEST_TYPE to declare a complex automation test.
+ * In contrast to simple tests, complex automation tests are called on multiple test-cases based on strings.
+ * This utility allows declaring those test-cases in a simple list instead of manually implementing the GetTests() function of the test class.
+ * Used like this:
+ * -------------------------
+ * OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_BEGIN(PrintString, DEFAULT_OUU_TEST_FLAGS)
+ * OUU_COMPLEX_AUTOMATION_TESTCASE("Hello")
+ * OUU_COMPLEX_AUTOMATION_TESTCASE("World")
+ * OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_END(PrintString)
+ * {
+ *     // test case implementation
+ *     // use parameter (const FString& Parameters) to get one of
+ *     // the test-case strings "Hello" and "World"
+ * }
+ * -------------------------
+ */
 #define OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_BEGIN(TestCase, TestFlags) \
 OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_BEGIN_INNER(OUU_TEST_CLASS(TestCase), OUU_TESTCASE_PRETTY_NAME(TestCase), TestFlags)
 
-   /**
-	* Allows defining a test-case without having to explicitly name it.
-	* Use this inline after OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_BEGIN()
-	*/
+/**
+ * Allows defining a test-case without having to explicitly name it.
+ * Use this inline after OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_BEGIN()
+ */
 #define OUU_COMPLEX_AUTOMATION_TESTCASE(TestCaseString) \
 OUU_COMPLEX_AUTOMATION_TESTCASE_INNER(TestCaseString)
 
-	/**
-	 * Allows defining a test-case together with a display name to go along with it.
-	 * Use this inline after OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_BEGIN()
-	 */
+/**
+ * Allows defining a test-case together with a display name to go along with it.
+ * Use this inline after OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_BEGIN()
+ */
 #define OUU_COMPLEX_AUTOMATION_TESTCASE_NAMED(DisplayName, TestCaseString) \
 OutTestCommands.Add(TestCaseString); \
 OutBeautifiedNames.Add(DisplayName);
 
-	 /**
-	  * End the header of a complex automation test definition.
-	  * After this comes the test function body in curly brackets.
-	  */
+/**
+ * End the header of a complex automation test definition.
+ * After this comes the test function body in curly brackets.
+ */
 #define OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_END(TestCase) \
 OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_END_INNER(OUU_TEST_CLASS(TestCase))
 
-	  // ---------------------------------
+// ---------------------------------
 
-	  // shared macros
+// shared macros
 #define OUU_CONCAT_TEST_CLASS_NAME(a, b, c, d) a ## b ## c ## d
 #define OUU_EXPAND_TEST_CLASS_NAME(TestType, TestCase) OUU_CONCAT_TEST_CLASS_NAME(F, TestType, Tests_, TestCase)
 #define OUU_TEST_CLASS(TestCase) OUU_EXPAND_TEST_CLASS_NAME(OUU_TEST_TYPE, TestCase)
@@ -117,5 +117,36 @@ void TestCaseClass::GetTests(TArray<FString>& OutBeautifiedNames, TArray<FString
 #define OUU_IMPLEMENT_COMPLEX_AUTOMATION_TEST_END_INNER(TestCaseClass) \
 } \
 bool TestCaseClass::RunTest(const FString& Parameters)
+
+// ---------------------------------
+
+/**
+ * Shorthands for calling test functions for use in specs.
+ * A detailed plain-text description should already be provided in the surrounding It(),
+ * so it's often even really hard to even provide a good description other than copying the object/function call
+ * that is tested, which is exactly what this macro does.
+ * Doesn't have any early exit conditions (like UTEST macros) because those obfuscate the test flow without adding
+ * any significant advantages.
+ */ 
+#define SPEC_TEST_TRUE(Actual) TestTrue(PREPROCESSOR_TO_STRING(Actual), Actual)
+#define SPEC_TEST_FALSE(Actual) TestFalse(PREPROCESSOR_TO_STRING(Actual), Actual)
+#define SPEC_TEST_EQUAL(Actual, Expected) TestEqual(WHAT_SPEC_TEST_COMPARE(Actual, Expected), Actual, Expected)
+#define SPEC_TEST_NOT_EQUAL(Actual, Expected) TestNotEqual(WHAT_SPEC_TEST_COMPARE(Actual, Expected), Actual, Expected)
+#define SPEC_TEST_EQUAL_TOLERANCE(Actual, Expected, Tolerance) TestEqual(WHAT_SPEC_TEST_COMPARE(Actual, Expected) " (tolerance: " PREPROCESSOR_TO_STRING(Tolerance) ")", Actual, Expected, Tolerance)
+#define SPEC_TEST_EQUAL_INSENSITIVE(Actual, Expected) TestEqualInsensitive(WHAT_SPEC_TEST_COMPARE(Actual, Expected), Actual, Expected)
+#define SPEC_TEST_SAME(Actual, Expected) TestSame(WHAT_SPEC_TEST_COMPARE(Actual, Expected), Actual, Expected)
+#define SPEC_TEST_NOT_SAME(Actual, Expected) TestNotSame(WHAT_SPEC_TEST_COMPARE(Actual, Expected), Actual, Expected)
+#define SPEC_TEST_VALID(Actual) TestValid(PREPROCESSOR_TO_STRING(Actual), Actual)
+#define SPEC_TEST_INVALID(Actual) TestInvalid(PREPROCESSOR_TO_STRING(Actual), Actual, Actual)
+#define SPEC_TEST_NULL(Actual) TestNull(PREPROCESSOR_TO_STRING(Actual), Actual, Actual)
+#define SPEC_TEST_NOT_NULL(Actual) TestNotNull(PREPROCESSOR_TO_STRING(Actual), Actual, Actual)
+#define SPEC_TEST_NOT_NULL(Actual) TestNotNull(PREPROCESSOR_TO_STRING(Actual), Actual, Actual)
+#define SPEC_TEST_ARRAYS_EQUAL(Actual, Expected) TestArraysEqual(*this, WHAT_SPEC_TEST_COMPARE(Actual, Expected), Actual, Expected)
+#define SPEC_TEST_ARRAYS_MATCH_UNORDERED(Actual, Expected) TestUnorderedArraysMatch(*this, WHAT_SPEC_TEST_COMPARE(Actual, Expected), Actual, Expected)
+
+// shared inner macros
+#define WHAT_SPEC_TEST_COMPARE(Actual, Expected) PREPROCESSOR_TO_STRING(Actual vs Expected)
+
+// ---------------------------------
 
 #endif
