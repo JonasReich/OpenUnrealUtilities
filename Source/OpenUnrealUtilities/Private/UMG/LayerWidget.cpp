d3227cfed30858c67c1b6da3122bcab71a59de12
Jonas Reich <mail@jonasreich.de>
2020-09-14 02:02:27 +0200

    Added layer widget and widget stack

---
+++ Source/OpenUnrealUtilities/Private/UMG/LayerWidget.cpp
@@ -0,0 +1,150 @@
+// Copyright (c) 2020 Jonas Reich
+
+#include "UMG/LayerWidget.h"
+#include "Widgets/SWidget.h"
+#include "Widgets/SViewport.h"
+#include "Framework/Application/SlateApplication.h"
+#include "OpenUnrealUtilities.h"
+#include "UMG/UMGUtils.h"
+#include "UMG/UMGInputBinding.h"
+
+void UOUULayerWidget::UpdateLayer(const UOUULayerWidget* LayerAbove)
+{
+	if (IsValid(LayerAbove))
+	{
+		bHasConcealingLayerAbove = (LayerAbove->IsActivelyConcealing() || LayerAbove->bHasConcealingLayerAbove);
+	}
+	else
+	{
+		bHasConcealingLayerAbove = false;
+	}
+
+	CheckLayerVisibility();
+	SetVisibility(GetDesiredVisibility());
+}
+
+bool UOUULayerWidget::IsActivelyConcealing() const
+{
+	return bConcealLayersBelow && IsVisible();
+}
+
+ESlateVisibility UOUULayerWidget::GetDesiredVisibility() const
+{
+	if (!IsConcealed() && bIsLayerVisible)
+	{
+		return IsLayerInputVisible() ? ESlateVisibility::Visible : ESlateVisibility::HitTestInvisible;
+	}
+	
+	return ESlateVisibility::Collapsed;
+}
+
+UUMGInputActionBindingStack* UOUULayerWidget::GetInputActionBindingStack()
+{
+	if (!bAllowInput)
+	{
+		UE_LOG(LogOpenUnrealUtilities, Warning, TEXT("GetInputActionBindingStack was called on Layer Widget '%s' that is not marked as bAllowInput!"));
+		return nullptr;
+	}
+	if (!IsValid(InputActionBindingStack))
+	{
+		InputActionBindingStack = UUMGInputActionBindingStack::CreateUMGInputActionBindingStack(this);
+	}
+	return InputActionBindingStack;
+}
+
+void UOUULayerWidget::NativeOnFocusChanging(const FWeakWidgetPath& PreviousFocusPath, const FWidgetPath& NewWidgetPath, const FFocusEvent& InFocusEvent)
+{
+	Super::NativeOnFocusChanging(PreviousFocusPath, NewWidgetPath, InFocusEvent);
+	auto SafeWidget = TakeWidget();
+
+	if (PreviousFocusPath.ContainsWidget(SafeWidget))
+		LastValidFocusPath = PreviousFocusPath;
+	if (!NewWidgetPath.ContainsWidget(SafeWidget))
+	{
+		// probable loss of focus on the layer
+		// we want to check if the new focus path is outside of the game viewport, because that is a common scenario in PIE
+		// during which we do NOT want the focus to be constantly reset
+		if (DoesPathContainGameViewport(NewWidgetPath))
+		{
+			OnRequestResetFocusFromTopLayer.Broadcast();
+		}
+	}
+}
+
+bool UOUULayerWidget::DoesPathContainGameViewport(const FWidgetPath& NewWidgetPath)
+{
+#if WITH_EDITOR
+	// In the editor we need to actually perform the check:
+	TSharedPtr<SWidget> GameViewport = StaticCastSharedPtr<SWidget>(FSlateApplication::Get().GetGameViewport());
+	return GameViewport.IsValid() && NewWidgetPath.ContainsWidget(GameViewport.ToSharedRef());
+#else
+	// Assume the game viewport is always part of a focus path in non-editor builds as there should only be the game:
+	return true;
+#endif
+}
+
+bool UOUULayerWidget::IsConcealed() const
+{
+	return bHasConcealingLayerAbove && bMayBeConcealedFromAbove;
+}
+
+void UOUULayerWidget::CheckLayerVisibility()
+{
+	if (bIsFocusable)
+	{
+		UE_LOG(LogOpenUnrealUtilities, Warning, TEXT("Layer widgets themselves must not be focusable! Made layer '%s' not focusable..."), *GetName());
+		bIsFocusable = false;
+	}
+
+	if (!IsConcealed())
+	{
+		bIsLayerVisible = false;
+		bIsLayerInputVisible = false;
+
+		UMGUtils::ForEachWidgetAndDescendants<const UWidget>(this, false, [&](const UWidget* W) -> bool
+		{
+			bIsLayerVisible |= W->IsVisible();
+			bIsLayerInputVisible = bAllowInput && UMGUtils::IsInputVisible(W);
+			
+			// break when we have all the info we need
+			return bIsLayerInputVisible || (!bAllowInput && bIsLayerVisible);
+		});
+	}
+	else
+	{
+		bIsLayerVisible = false;
+		bIsLayerInputVisible = false;
+	}
+}
+
+bool UOUULayerWidget::IsLayerInputVisible() const
+{
+	return bIsLayerInputVisible;
+}
+
+bool UOUULayerWidget::ResetUserFocus_Implementation()
+{
+	if (LastValidFocusPath.IsValid())
+	{
+		FWidgetPath WidgetPath = LastValidFocusPath.ToWidgetPath();
+		int32 FocusableWidgetIdx = WidgetPath.Widgets.FindLastByPredicate([](const FArrangedWidget& WidgetPathElement) -> bool
+		{
+			return WidgetPathElement.Widget->SupportsKeyboardFocus();
+		});
+		if (FocusableWidgetIdx != INDEX_NONE)
+		{
+			TSharedPtr<SWidget> SafeWidget = WidgetPath.Widgets[FocusableWidgetIdx].Widget;
+			if (ULocalPlayer* LocalPlayer = GetOwningLocalPlayer())
+			{
+				TOptional<int32> UserIndex = FSlateApplication::Get().GetUserIndexForController(LocalPlayer->GetControllerId());
+				if (UserIndex.IsSet())
+				{
+					FReply& DelayedSlateOperations = LocalPlayer->GetSlateOperations();
+					return FSlateApplication::Get().SetUserFocus(UserIndex.GetValue(), SafeWidget);
+				}
+			}
+		}
+	}
+
+	return ResetUserFocus_TemplateImplementation();
+}
---
+++ Source/OpenUnrealUtilitiesTests/Private/UMG/LayerStackWidgetTests.h
@@ -0,0 +1,86 @@
+// Copyright (c) 2020 Jonas Reich
+
+#include "UMG/LayerStackWidget.h"
+#include "UMG/LayerWidget.h"
+#include "Components/Overlay.h"
+#include "LayerStackWidgetTests.generated.h"
+
+// Test layer stack that doesn't have any layers
+UCLASS(meta = (Hidden, HideDropDown))
+class UTestUOUULayerStackWidget_Empty : public UOUULayerStackWidget
+{
+	GENERATED_BODY()
+protected:
+	void NativeOnInitialized() override
+	{
+		WidgetStack = WidgetTree->ConstructWidget<UOverlay>();
+		WidgetTree->RootWidget = WidgetStack;
+
+		Super::NativeOnInitialized();
+	}
+};
+
+// Test layer that tracks the last layer above that was passed via UpdateLayer() 
+UCLASS(meta = (Hidden, HideDropDown))
+class UOUULayerStackTickTestLayer : public UOUULayerWidget
+{
+	GENERATED_BODY()
+public:
+	// params: self layer, last layer above
+	DECLARE_EVENT_TwoParams(UOUULayerStackTickTestLayer, FOnLayerUpdated, const UOUULayerWidget*, const UOUULayerWidget*);
+	FOnLayerUpdated OnLayerUpdated;
+
+	virtual void UpdateLayer(const UOUULayerWidget* LayerAbove) override
+	{
+		OnLayerUpdated.Broadcast(this, LayerAbove);
+		Super::UpdateLayer(LayerAbove);
+	}
+};
+
+// Test layer stack that has three layers (top-down: top, middle, bottom)
+UCLASS(meta = (Hidden, HideDropDown))
+class UTestUOUULayerStackWidget : public UOUULayerStackWidget
+{
+	GENERATED_BODY()
+public:
+	UOUULayerStackTickTestLayer* TopLayer;
+	UOUULayerStackTickTestLayer* MiddleLayer;
+	UOUULayerStackTickTestLayer* BottomLayer;
+
+	static TArray<const UOUULayerWidget*> UpdatedLayers;
+	static TArray<const UOUULayerWidget*> LayersAboveUpdatedLayers;
+
+	static void ClearUpdatedLayers()
+	{
+		UpdatedLayers.Empty();
+		LayersAboveUpdatedLayers.Empty();
+	}
+
+protected:
+	UOUULayerStackTickTestLayer* CreateLayer()
+	{
+		auto* NewLayer = WidgetTree->ConstructWidget<UOUULayerStackTickTestLayer>();
+		WidgetStack->AddChildToOverlay(NewLayer);
+		NewLayer->OnLayerUpdated.AddUObject(this, &UTestUOUULayerStackWidget::HandleLayerUpdated);
+		return NewLayer;
+	}
+
+	void NativeOnInitialized() override
+	{
+		// This would normally happen in the blueprint designer, not at runtime!
+		WidgetStack = WidgetTree->ConstructWidget<UOverlay>();
+		WidgetTree->RootWidget = WidgetStack;
+		TopLayer = CreateLayer();
+		MiddleLayer = CreateLayer();
+		BottomLayer = CreateLayer();
+
+		Super::NativeOnInitialized();
+	}
+
+	UFUNCTION()
+	void HandleLayerUpdated(const UOUULayerWidget* LayerUpdated, const UOUULayerWidget* LayerAbove)
+	{
+		UpdatedLayers.Add(LayerUpdated);
+		LayersAboveUpdatedLayers.Add(LayerAbove);
+	}
+};
