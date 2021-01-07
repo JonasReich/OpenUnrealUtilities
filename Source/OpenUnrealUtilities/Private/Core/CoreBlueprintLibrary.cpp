7f4fe3c5885bba0ad7250de434b6906521e57aec
Jonas Reich <mail@jonasreich.de>
2021-01-07 01:04:32 +0100

    Added CoreBlueprintLibrary to expose some more core functions like GetDefaultObject to blueprints

---
+++ Source/OpenUnrealUtilities/Private/Core/CoreBlueprintLibrary.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#include "Core/CoreBlueprintLibrary.h"
+
+UObject* UCoreBlueprintLibrary::GetClassDefaultObject(TSubclassOf<UObject> Class)
+{
+	return Class != nullptr ? GetMutableDefault<UObject>(Class) : nullptr;
+}
+
+UObject* UCoreBlueprintLibrary::GetClassDefaultObjectFromObject(UObject* Object)
+{
+	return IsValid(Object) ? GetMutableDefault<UObject>(Object->GetClass()) : nullptr;
+}
+
+UWorld* UCoreBlueprintLibrary::TryGetWorldFromObject(UObject* WorldContextObject)
+{
+	return GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::ReturnNull);
+}
+
+UWorld* UCoreBlueprintLibrary::TryGetWorldFromObject_K2(UObject* WorldContextObject)
+{
+	return TryGetWorldFromObject(WorldContextObject);
+}
---
+++ Source/OpenUnrealUtilities/Public/Core/CoreBlueprintLibrary.h
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CoreBlueprintLibrary.generated.h"
+
+UCLASS()
+class UCoreBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	/** @returns the mutable class default object of the specified class. Proceed with caution! */
+	UFUNCTION(BlueprintPure)
+	static UObject* GetClassDefaultObject(TSubclassOf<UObject> Class);
+
+	/** @returns the mutable class default object of the objects class. Proceed with caution! */
+	UFUNCTION(BlueprintPure)
+    static UObject* GetClassDefaultObjectFromObject(UObject* Object);
+
+	/**
+	 * Attempts to get the world from a world context object.
+	 * Returns nullptr if no world was found.
+	 */
+	UFUNCTION(BlueprintPure)
+	static UWorld* TryGetWorldFromObject(UObject* WorldContextObject);
+
+	/**
+	 * Attempts to get the world from the current blueprint context.
+	 * Functionally the same as TryGetWorldFromObject(), but this version auto-populates the WorldContextObject parameter in Blueprints.
+	 * Returns nullptr if not world was found.
+	 */
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Try Get World", WorldContext = "WorldContextObject"))
+	static UWorld* TryGetWorldFromObject_K2(UObject* WorldContextObject);
+};
