2d3afefe98476f4c29ea74cd8e90a9f0e3b3c323
Jonas Reich <mail@jonasreich.de>
2021-01-17 22:00:49 +0100

    Added SlateNotificationLibrary

---
+++ Source/OUURuntime/Private/Slate/SlateNotificationLibrary.cpp
@@ -0,0 +1,198 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#include "Slate/SlateNotificationLibrary.h"
+
+
+#include "OUURuntimeModule.h"
+#include "Components/Widget.h"
+#include "Framework/Notifications/NotificationManager.h"
+#include "Widgets/Notifications/SNotificationList.h"
+
+FNotificationInfo FSlateNotificationInfo::ToNativeNotificationInfo() const
+{
+	FNotificationInfo Result(Text);
+	if (bShowImage)
+	{
+		Result.Image = new FSlateBrush(Image);
+	}
+	Result.bUseThrobber = bUseThrobber;
+	Result.bUseSuccessFailIcons = bUseSuccessFailIcons;
+	Result.bUseLargeFont = bUseLargeFont;
+	if (WidthOverride > 0.f)
+	{
+		Result.WidthOverride = WidthOverride; 
+	}
+	Result.bFireAndForget = bFireAndForget;
+	return Result;
+}
+
+void FSlateNotificationHandle::NewGuid()
+{
+	FPlatformMisc::CreateGuid(NotificationId);
+}
+
+bool FSlateNotificationHandle::IsValid() const
+{
+	return NotificationId.IsValid();
+}
+
+bool FSlateNotificationHandle::operator==(const FSlateNotificationHandle& Other) const
+{
+	return Other.NotificationId == NotificationId;
+}
+
+FSlateNotificationHandle FSlateNotificationHandle::InvalidHandle()
+{
+	return FSlateNotificationHandle();
+}
+
+uint32 GetTypeHash(const FSlateNotificationHandle& Handle)
+{
+	return GetTypeHash(Handle.NotificationId);
+}
+
+ESlateNotificationState SlateNotificationState_ConvertSlateToBlueprint(SNotificationItem::ECompletionState State)
+{
+	switch(State)
+	{
+	case SNotificationItem::CS_Pending: return ESlateNotificationState::Pending;
+	case SNotificationItem::CS_Success: return ESlateNotificationState::Success;
+	case SNotificationItem::CS_Fail: return ESlateNotificationState::Fail;
+	default: return ESlateNotificationState::None;
+	}
+}
+
+SNotificationItem::ECompletionState SlateNotificationState_ConvertBlueprintToSlate(ESlateNotificationState State)
+{
+	switch(State)
+	{
+	case ESlateNotificationState::Pending: return SNotificationItem::CS_Pending;
+	case ESlateNotificationState::Success: return SNotificationItem::CS_Success;
+	case ESlateNotificationState::Fail: return SNotificationItem::CS_Fail;
+	default: return SNotificationItem::CS_None;
+	}
+}
+
+FSlateNotificationHandle USlateNotificationLibrary::AddSlateNotification(const FSlateNotificationInfo& Info)
+{
+	FSlateNotificationHandle Handle;
+	while(!Handle.IsValid() || GetNotificationMap().Contains(Handle))
+	{
+		Handle.NewGuid();
+	}
+	if(!IsInGameThread())
+	{
+		UE_LOG(LogOpenUnrealUtilities, Warning, TEXT("Failed to create slate notification, "
+			"because AddSlateNotification was called from a thread that is not the game thread."));
+		return FSlateNotificationHandle::InvalidHandle();
+	}
+	const auto NewNotification = FSlateNotificationManager::Get().AddNotification(Info.ToNativeNotificationInfo());
+	GetNotificationMap().Add(Handle, NewNotification);
+	return Handle;
+}
+
+void USlateNotificationLibrary::SetSlateNotificationText(const FSlateNotificationHandle& NotificationHandle, FText Text)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		Notification->SetText(Text);
+	}
+}
+
+void USlateNotificationLibrary::SetSlateNotificationHyperlink(const FSlateNotificationHandle& NotificationHandle, FText HyperlinkText, FSlateNotificationSimpleSingleCastDelegate HyperlinkDelegate)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		const FSimpleDelegate Delegate = FSimpleDelegate::CreateLambda([=]()
+		{
+			HyperlinkDelegate.ExecuteIfBound();	
+		});
+		Notification->SetHyperlink(Delegate, HyperlinkText);
+	}
+}
+
+void USlateNotificationLibrary::SetSlateNotificationExpireDuration(const FSlateNotificationHandle& NotificationHandle, float ExpireDuration)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		Notification->SetExpireDuration(ExpireDuration);
+	}
+}
+
+void USlateNotificationLibrary::SetSlateNotificationFadeInDuration(const FSlateNotificationHandle& NotificationHandle, float FadeInDuration)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		Notification->SetFadeInDuration(FadeInDuration);
+	}
+}
+
+void USlateNotificationLibrary::SetSlateNotificationFadeOutDuration(const FSlateNotificationHandle& NotificationHandle, float FadeOutDuration)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		Notification->SetFadeOutDuration(FadeOutDuration);
+	}
+}
+
+ESlateNotificationState USlateNotificationLibrary::GetSlateNotificationCompletionState(const FSlateNotificationHandle& NotificationHandle)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		return SlateNotificationState_ConvertSlateToBlueprint(Notification->GetCompletionState());
+	}
+	return ESlateNotificationState::None;
+}
+
+void USlateNotificationLibrary::SetSlateNotificationCompletionState(const FSlateNotificationHandle& NotificationHandle, ESlateNotificationState CompletionState)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		Notification->SetCompletionState(SlateNotificationState_ConvertBlueprintToSlate(CompletionState));
+	}
+}
+
+void USlateNotificationLibrary::ExpireSlateNotificationAndFadeout(const FSlateNotificationHandle& NotificationHandle)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		Notification->ExpireAndFadeout();
+	}
+}
+
+void USlateNotificationLibrary::FadeoutSlateNotification(const FSlateNotificationHandle& NotificationHandle)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		Notification->Fadeout();
+	}
+}
+
+void USlateNotificationLibrary::PulseSlateNotification(const FSlateNotificationHandle& NotificationHandle, FLinearColor GlowColor)
+{
+	if (auto* Notification = FindNotification(NotificationHandle))
+	{
+		Notification->Pulse(GlowColor);
+	}
+}
+
+void USlateNotificationLibrary::ReleaseSlateNotificationHandle(const FSlateNotificationHandle& NotificationHandle)
+{
+	GetNotificationMap().Remove(NotificationHandle);
+}
+
+USlateNotificationLibrary::FNotificationMap& USlateNotificationLibrary::GetNotificationMap()
+{
+	return GetMutableDefault<USlateNotificationLibrary>()->RegisteredNotifications;
+}
+
+SNotificationItem* USlateNotificationLibrary::FindNotification(const FSlateNotificationHandle& Handle)
+{
+	UE_CLOG(!Handle.IsValid(), LogOpenUnrealUtilities, Warning, TEXT("Slate notification handle is invalid"));
+	if (auto* Ptr = GetNotificationMap().Find(Handle))
+	{
+		return Ptr->Get();
+	}
+	UE_LOG(LogOpenUnrealUtilities, Warning, TEXT("Failed to find notification."));
+	return nullptr;
+}
---
+++ Source/OUURuntime/Public/Slate/SlateNotificationLibrary.h
@@ -0,0 +1,140 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Widgets/Notifications/SNotificationList.h"
+
+#include "SlateNotificationLibrary.generated.h"
+
+struct FNotificationInfo;
+class SNotificationItem;
+
+DECLARE_DYNAMIC_DELEGATE(FSlateNotificationSimpleSingleCastDelegate);
+
+USTRUCT(BlueprintType)
+struct FSlateNotificationInfo
+{
+	GENERATED_BODY()
+public:
+	/** The text displayed in the notification */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification")
+	FText Text;
+
+	/** The fade in duration for the notification */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification|Timing")
+	float FadeInDuration;
+
+	/** The fade out duration for the notification */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification|Timing")
+	float FadeOutDuration;
+
+	/** The duration before a fadeout for the notification */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification|Timing")
+	float ExpireDuration;
+
+	/** When true an image is displayed next to the text */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification")
+	bool bShowImage;
+	
+	/** The icon image to display next to the text */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification")
+	FSlateBrush Image;
+	
+	/** Controls whether or not to add the animated throbber */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification|Styling")
+	bool bUseThrobber;
+
+	/** Controls whether or not to display the success and fail icons */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification|Styling")
+	bool bUseSuccessFailIcons;
+
+	/** When true the larger bolder font will be used to display the message */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification|Styling")
+	bool bUseLargeFont;
+
+	/** When set to a bigger value than zero this forces the width of the box, used to stop resizing on text change */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification|Styling")
+	float WidthOverride;
+
+	/** When true the notification will automatically time out after the expire duration. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slate Notification|Misc")
+	bool bFireAndForget;
+
+	FNotificationInfo ToNativeNotificationInfo() const;
+};
+
+USTRUCT(BlueprintType)
+struct OUURUNTIME_API FSlateNotificationHandle
+{
+	GENERATED_BODY()
+public:
+	friend OUURUNTIME_API uint32 GetTypeHash(const FSlateNotificationHandle&);
+
+	void NewGuid();
+	bool IsValid() const;
+
+	bool operator==(const FSlateNotificationHandle& Other) const;
+
+	static FSlateNotificationHandle InvalidHandle();
+
+private:
+	/** integer handle that is use to identify registered notifications */
+	FGuid NotificationId = FGuid();	
+};
+
+OUURUNTIME_API uint32 GetTypeHash(const FSlateNotificationHandle& Handle);
+
+UENUM(BlueprintType)
+enum class ESlateNotificationState : uint8
+{
+	None,
+	Pending,
+	Success,
+	Fail
+};
+
+OUURUNTIME_API ESlateNotificationState SlateNotificationState_ConvertSlateToBlueprint(SNotificationItem::ECompletionState State);
+OUURUNTIME_API SNotificationItem::ECompletionState SlateNotificationState_ConvertBlueprintToSlate(ESlateNotificationState State);
+
+UCLASS()
+class USlateNotificationLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	static FSlateNotificationHandle AddSlateNotification(const FSlateNotificationInfo& Info);
+
+	UFUNCTION(BlueprintCallable)
+	static void SetSlateNotificationText(const FSlateNotificationHandle& NotificationHandle, FText Text);
+	UFUNCTION(BlueprintCallable)
+	static void SetSlateNotificationHyperlink(const FSlateNotificationHandle& NotificationHandle, FText HyperlinkText, FSlateNotificationSimpleSingleCastDelegate HyperlinkDelegate);
+	UFUNCTION(BlueprintCallable)
+	static void SetSlateNotificationExpireDuration(const FSlateNotificationHandle& NotificationHandle, float ExpireDuration);
+	UFUNCTION(BlueprintCallable)
+	static void SetSlateNotificationFadeInDuration(const FSlateNotificationHandle& NotificationHandle, float FadeInDuration);
+	UFUNCTION(BlueprintCallable)
+	static void SetSlateNotificationFadeOutDuration(const FSlateNotificationHandle& NotificationHandle, float FadeOutDuration);
+
+	UFUNCTION(BlueprintPure)
+	static ESlateNotificationState GetSlateNotificationCompletionState(const FSlateNotificationHandle& NotificationHandle);
+	UFUNCTION(BlueprintCallable)
+	static void SetSlateNotificationCompletionState(const FSlateNotificationHandle& NotificationHandle, ESlateNotificationState CompletionState);
+
+	UFUNCTION(BlueprintCallable)
+	static void ExpireSlateNotificationAndFadeout(const FSlateNotificationHandle& NotificationHandle);
+	UFUNCTION(BlueprintCallable)
+	static void FadeoutSlateNotification(const FSlateNotificationHandle& NotificationHandle);
+	UFUNCTION(BlueprintCallable)
+	static void PulseSlateNotification(const FSlateNotificationHandle& NotificationHandle, FLinearColor GlowColor);
+
+	UFUNCTION(BlueprintCallable)
+	static void ReleaseSlateNotificationHandle(const FSlateNotificationHandle& NotificationHandle);
+
+private:
+	using FNotificationMap = TMap<FSlateNotificationHandle, TSharedPtr<SNotificationItem>>;
+	FNotificationMap RegisteredNotifications;
+	static FNotificationMap& GetNotificationMap();
+	static SNotificationItem* FindNotification(const FSlateNotificationHandle& Handle);
+};
