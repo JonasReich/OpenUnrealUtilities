0d9c0fe1ce59b8fcc453e476f6bc3ba071ab0f32
Jonas Reich <mail@jonasreich.de>
2021-02-15 01:06:50 +0100

    Added more utility functions to array utils

--- Source/OUURuntime/Public/Templates/ArrayUtils.h
+++ Source/OUURuntime/Public/Templates/ArrayUtils.h
@@ -27,4 +27,108 @@ public:
 		Array.SetNum(Num);
 		SetAllTo(Array, Value);
 	}
+
+	/**
+	 * Removes and returns an element from the array from a specified index.
+	 */
+	template<typename ElementType, typename AllocatorType>
+	static ElementType TakeAt(TArray<ElementType, AllocatorType>& Array, int32 Index)
+	{
+		check(Array.Num() > 0);
+		ElementType Result = Array[Index];
+		Array.RemoveAt(Index);
+		return Result;
+	}
+
+	/**
+	 * Removes and returns an element from the array from a specified index using RemoveAtSwap function.
+	 * This version is much more efficient than TakeAt (O(Count) instead of
+	 * O(ArrayNum)), but does not preserve the order.
+	 */
+	template<typename ElementType, typename AllocatorType>
+    static ElementType TakeAtSwap(TArray<ElementType, AllocatorType>& Array, int32 Index)
+	{
+		check(Array.Num() > 0);
+		ElementType Result = Array[Index];
+		Array.RemoveAtSwap(Index);
+		return Result;
+	}
+
+	/**
+	 * Get a random array element based on a deterministic random stream sample.
+	 * The array must contain at least one element!
+	 */
+	template<typename ElementType, typename AllocatorType>
+    static ElementType GetRandomElement(const TArray<ElementType, AllocatorType>& Array, FRandomStream& Stream)
+	{
+		check(Array.Num() > 0);
+		return Array[Stream.RandHelper(Array.Num())];
+	}
+
+	/**
+	 * Get a random array element based on a non-deterministic FMath random sample.
+	 * The array must contain at least one element!
+	 */
+	template<typename ElementType, typename AllocatorType>
+    static ElementType GetRandomElement(const TArray<ElementType, AllocatorType>& Array)
+	{
+		check(Array.Num() > 0);
+		return Array[FMath::RandRange(0, Array.Num() - 1)];
+	}
+
+	/**
+	 * Remove and return a random element from the array.
+	 * Randomness is deterministic based on random stream.
+	 * The array must contain at least one element!
+	 */
+	template<typename ElementType, typename AllocatorType>
+    static ElementType TakeRandomElement(TArray<ElementType, AllocatorType>& Array, FRandomStream& Stream)
+	{
+		check(Array.Num() > 0);
+		const int32 Index = Stream.RandHelper(Array.Num());
+		return TakeAt(Array, Index);
+	}
+
+	/**
+	 * Remove and return a random element from the array.
+	 * Randomness is deterministic based on random stream.
+	 * The array must contain at least one element!
+	 * This version is much more efficient than TakeRandomElement (O(Count) instead of
+	 * O(ArrayNum)), but does not preserve the order.
+	 */
+	template<typename ElementType, typename AllocatorType>
+    static ElementType TakeRandomElementSwap(TArray<ElementType, AllocatorType>& Array, FRandomStream& Stream)
+	{
+		check(Array.Num() > 0);
+		const int32 Index = Stream.RandHelper(Array.Num());
+		return TakeAtSwap(Array, Index);
+	}
+
+	/**
+	 * Remove and return a random element from the array.
+	 * Randomness is non-deterministic based on FMath random.
+	 * The array must contain at least one element!
+	 */
+	template<typename ElementType, typename AllocatorType>
+    static ElementType TakeRandomElement(TArray<ElementType, AllocatorType>& Array)
+	{
+		check(Array.Num() > 0);
+		const int32 Index = FMath::RandRange(0, Array.Num() - 1);
+		return TakeAt(Array, Index);
+	}
+
+	/**
+	 * Remove and return a random element from the array.
+	 * Randomness is non-deterministic based on FMath random.
+	 * The array must contain at least one element!
+	 * This version is much more efficient than TakeRandomElement (O(Count) instead of
+	 * O(ArrayNum)), but does not preserve the order.
+	 */
+	template<typename ElementType, typename AllocatorType>
+    static ElementType TakeRandomElementSwap(TArray<ElementType, AllocatorType>& Array)
+	{
+		check(Array.Num() > 0);
+		const int32 Index = FMath::RandRange(0, Array.Num() - 1);
+		return TakeAtSwap(Array, Index);
+	}
 };
--- Source/OUUTests/Private/Templates/ArrayUtils.spec.cpp
+++ Source/OUUTests/Private/Templates/ArrayUtils.spec.cpp
@@ -36,6 +36,131 @@ void FArrayUtilsSpec::Define()
 			TestArraysEqual(*this, "All elements are set to the same value, count was increased", WorkingArray, { 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 });
 		});
 	});
+
+	Describe("TakeAt", [this]()
+	{
+		It("should remove the correct element from the array", [this]()
+		{
+			TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+			const int32 Element = FArrayUtils::TakeAt(SourceArray, 2);
+			SPEC_TEST_EQUAL(Element, 3);
+			SPEC_TEST_TRUE(SourceArray.Num() == 4);
+		});
+
+		It("should retain the correct order of remaining elements", [this]()
+        {
+            TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+            FArrayUtils::TakeAt(SourceArray, 2);
+			TestArraysEqual(*this, "TestArray", SourceArray, TArray<int32>{1, 2, 4, 5});
+        });
+	});
+
+	Describe("TakeAtSwap", [this]()
+	{
+		It("should remove the correct element from the array", [this]()
+        {
+            TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+            const int32 Element = FArrayUtils::TakeAtSwap(SourceArray, 2);
+            SPEC_TEST_EQUAL(Element, 3);
+            SPEC_TEST_TRUE(SourceArray.Num() == 4);
+        });
+
+        It("should leave the correct elements in unspecified order", [this]()
+        {
+            TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+            FArrayUtils::TakeAtSwap(SourceArray, 2);
+            TestUnorderedArraysMatch(*this, "TestArray", SourceArray, TArray<int32>{1, 2, 4, 5});
+        });
+	});
+
+	Describe("GetRandomElement", [this]()
+	{
+		It("should never cause any access violations when called many (100) times", [this]()
+		{
+			const TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+			for (int32 i = 0; i < 100; i++)
+			{
+				FArrayUtils::GetRandomElement(SourceArray);
+			}
+		});
+
+		It("should always return the same element when used with the same seed/random stream", [this]()
+		{
+			const TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+			for (int32 i = 0; i < 5; i++)
+            {
+				int32 LastIterationResult = INDEX_NONE;
+				for (int32 j = 0; j < 5; j++)
+				{
+					FRandomStream Stream = i;
+					const int32 CurrentResult = FArrayUtils::GetRandomElement(SourceArray, Stream);
+					if (LastIterationResult == INDEX_NONE)
+					{
+						LastIterationResult = CurrentResult;
+					}
+					else
+					{
+						SPEC_TEST_EQUAL(LastIterationResult, CurrentResult);
+					}
+				}
+            }
+		});
+	});
+
+	Describe("TakeRandomElement", [this]()
+	{
+		It("should remove the returned element from the array", [this]()
+        {
+            TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+            const int32 Element = FArrayUtils::TakeRandomElement(SourceArray);
+            SPEC_TEST_FALSE(SourceArray.Contains(Element));
+            SPEC_TEST_TRUE(SourceArray.Num() == 4);
+        });
+
+		It("should completely empty the array when invoked array-length times", [this]()
+		{
+			TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+			for (int32 i = 0; i < 5; i++)
+			{
+				FArrayUtils::TakeRandomElement(SourceArray);
+			}
+			SPEC_TEST_EQUAL(SourceArray.Num(), 0);
+		});
+
+		It("should always return the same element when used with the same seed/random stream", [this]()
+        {
+            const TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+            for (int32 i = 0; i < 5; i++)
+            {
+                int32 LastIterationResult = INDEX_NONE;
+                for (int32 j = 0; j < 5; j++)
+                {
+                	TArray<int32> SourceArrayCopy = SourceArray;
+                    FRandomStream Stream = i;
+					const int32 CurrentResult = FArrayUtils::TakeRandomElement(SourceArrayCopy, Stream);
+                    if (LastIterationResult == INDEX_NONE)
+                    {
+                        LastIterationResult = CurrentResult;
+                    }
+                    else
+                    {
+                        SPEC_TEST_EQUAL(CurrentResult, LastIterationResult);
+                    }
+                }
+            }
+        });
+
+		It("should retain the correct order of remaining elements", [this]()
+        {
+            const TArray<int32> SourceArray = {1, 2, 3, 4, 5};
+            TArray<int32> SourceArrayCopy = SourceArray;
+			TArray<int32> SourceArrayREferenceCopy = SourceArray;
+            const int32 Element = FArrayUtils::TakeRandomElement(SourceArrayCopy);
+			const int32 ElementIndex = SourceArray.Find(Element);
+            SourceArrayREferenceCopy.RemoveAt(ElementIndex);
+            TestArraysEqual(*this, "TestArray", SourceArrayREferenceCopy, SourceArrayCopy);
+        });
+	});
 }
 
 #endif
