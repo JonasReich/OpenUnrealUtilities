034c1d11cad1e1db8a6ec41df5b4f8e8eab5200e
Jonas Reich <mail@jonasreich.de>
2021-04-30 20:20:40 +0200

    Added draft of SpiralIdUtilities

---
+++ Source/OUURuntime/Private/Math/SpiralIdUtilities.cpp
@@ -0,0 +1,75 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#include "Math/SpiralIdUtilities.h"
+
+int32 USpiralIdUtilities::ConvertCoordinatesToSpiralId(const int32 X, const int32 Y)
+{
+	const int32 A = FMath::Abs(FMath::Abs(X) - FMath::Abs(Y)) + FMath::Abs(X) + FMath::Abs(Y);
+	const int32 Sign = StaticCast<int32>(FMath::Sign<double>(0.1 + X + Y));
+	return A * A + Sign * (A + X - Y);
+}
+
+int32 USpiralIdUtilities::ConvertWorldLocation2DToSpiralId(const FVector2D& Location, float GridSize)
+{
+	return ConvertCoordinatesToSpiralId(
+		FMath::FloorToInt(Location.X / GridSize),
+		FMath::FloorToInt(-Location.Y / GridSize));
+}
+
+int32 USpiralIdUtilities::ConvertWorldLocationToSpiralId(const FVector& Location, float GridSize)
+{
+	return ConvertCoordinatesToSpiralId(
+		FMath::FloorToInt(Location.X / GridSize),
+		FMath::FloorToInt(-Location.Y / GridSize));
+}
+
+FIntPoint USpiralIdUtilities::ConvertSpiralIdToCoordinates(const int32 SpiralId)
+{
+	FIntPoint PerStepDelta(0, 1);
+	int32 SegmentLength = 1;
+
+	FIntPoint Coordinates(0, 0);
+	int32 SegmentProgress = 0;
+	for (int i = 0; i < SpiralId; ++i)
+	{
+		Coordinates += PerStepDelta;
+		++SegmentProgress;
+
+		if (SegmentProgress == SegmentLength)
+		{
+			SegmentProgress = 0;
+
+			// Rotate PerStepDelta by 90 deg
+			const int32 Temp = PerStepDelta.X;
+			PerStepDelta.X = -PerStepDelta.Y;
+			PerStepDelta.Y = Temp;
+
+			if (PerStepDelta.X == 0)
+			{
+				++SegmentLength;
+			}
+		}
+	}
+	return Coordinates;
+}
+
+FVector2D USpiralIdUtilities::ConvertSpiralIdToCenterLocation(const int32 SpiralId, float GridSize)
+{
+	FVector2D Result(0.5f, -0.5f);
+	Result += ConvertSpiralIdToCoordinates(SpiralId);
+	Result *= GridSize;
+	return Result;
+}
+
+FBox2D USpiralIdUtilities::ConvertSpiralIdToBounds(int32 SpiralId, float GridSize)
+{
+	const FIntPoint MinPoint = ConvertSpiralIdToCoordinates(SpiralId);
+	const FIntPoint MaxPoint = MinPoint + FIntPoint(1,-1);
+	return {MinPoint * GridSize, MaxPoint * GridSize};
+}
+
+FBox USpiralIdUtilities::ConvertSpiralIdToBounds3D(int32 SpiralId, float GridSize, float BoundsHeight)
+{
+	const FBox2D Box2D = ConvertSpiralIdToBounds(SpiralId, GridSize);
+	return FBox (FVector(Box2D.Min.X, Box2D.Min.Y, 0), FVector(Box2D.Max.X, Box2D.Max.Y, 0) + FVector(0,0,BoundsHeight));
+}
---
+++ Source/OUURuntime/Public/Math/SpiralIdUtilities.h
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SpiralIdUtilities.generated.h"
+
+/**
+ * Utility functions for converting coordinates to spiral IDs and vice versa.
+ * Spiral IDs allow indexing a 2D grid with only positive numbers starting at a center location.
+ * The resulting grid looks somewhat like this (notice flipped Y axis like in UE4 when looking down)
+ *
+ *    -2  -1    0   +1   +2
+ * -2 +----+----+----+----+
+ *    | 15 | 04 | 05 | 06 |
+ * -1 +----+----+----+----+
+ *    | 14 | 03 | 00 | 07 |
+ *  0 +----+----o----+----+
+ *    | 13 | 02 | 01 | 08 |
+ * +1 +----+----+----+----+
+ *    | 12 | 11 | 10 | 09 |
+ * +2 +----+----+----+----+
+ */
+UCLASS(BlueprintType)
+class OUURUNTIME_API USpiralIdUtilities : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Convert grid coordinates to spiral coordinates the same way it is calculated in Houdini
+	 * Note that this uses grid coordinates as in grid cells, so the world coordinates will likely be different from X and Y.
+	 */
+	UFUNCTION(BlueprintPure)
+	static int32 ConvertCoordinatesToSpiralId(const int32 X, const int32 Y);
+
+	UFUNCTION(BlueprintPure)
+	static int32 ConvertWorldLocation2DToSpiralId(const FVector2D& Location, float GridSize);
+
+	UFUNCTION(BlueprintPure)
+	static int32 ConvertWorldLocationToSpiralId(const FVector& Location, float GridSize);
+
+	UFUNCTION(BlueprintPure)
+	static FIntPoint ConvertSpiralIdToCoordinates(const int32 SpiralId);
+
+	UFUNCTION(BlueprintPure)
+	static FVector2D ConvertSpiralIdToCenterLocation(const int32 SpiralId, float GridSize);
+
+	UFUNCTION(BlueprintPure)
+	static FBox2D ConvertSpiralIdToBounds(int32 SpiralId, float GridSize);
+
+	UFUNCTION(BlueprintPure)
+	static FBox ConvertSpiralIdToBounds3D(int32 SpiralId, float GridSize, float BoundsHeight);
+};
