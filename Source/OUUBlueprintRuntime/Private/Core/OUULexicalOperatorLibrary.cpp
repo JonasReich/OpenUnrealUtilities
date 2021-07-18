1e40b0a7bebec43732e8fd7b995f94d59f190ff4
Jonas Reich <mail@jonasreich.de>
2021-07-18 17:21:28 +0200

    Exposed lexical comparison operators to Blueprint

---
+++ Source/OUUBlueprintRuntime/Private/Core/OUULexicalOperatorLibrary.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#include "Core/OUULexicalOperatorLibrary.h"
+
+bool UOUULexicalOperatorLibrary::Less_StringString(const FString& A, const FString& B)
+{
+	return A < B;
+}
+
+bool UOUULexicalOperatorLibrary::Greater_StringString(const FString& A, const FString& B)
+{
+	return A > B;
+}
+
+bool UOUULexicalOperatorLibrary::LessEqual_StringString(const FString& A, const FString& B)
+{
+	return A <= B;
+}
+
+bool UOUULexicalOperatorLibrary::GreaterEqual_StringString(const FString& A, const FString& B)
+{
+	return A >= B;
+}
+
+bool UOUULexicalOperatorLibrary::Less_NameName(const FName& A, const FName& B)
+{
+	return FNameLexicalLess().operator()(A, B);
+}
+
+bool UOUULexicalOperatorLibrary::Greater_NameName(const FName& A, const FName& B)
+{
+	return !LessEqual_NameName(A, B);
+}
+
+bool UOUULexicalOperatorLibrary::LessEqual_NameName(const FName& A, const FName& B)
+{
+	return (A == B) || Less_NameName(A, B);
+}
+
+bool UOUULexicalOperatorLibrary::GreaterEqual_NameName(const FName& A, const FName& B)
+{
+	return (A != B) && (!Less_NameName(A, B)); 
+}
---
+++ Source/OUUBlueprintRuntime/Public/Core/OUULexicalOperatorLibrary.h
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OUULexicalOperatorLibrary.generated.h"
+
+/**
+ * Comparison operators for lexical core types (FString, FName).
+ */
+UCLASS()
+class UOUULexicalOperatorLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	// FString
+
+	/** Returns true if A is lexically less than B (A < B) */
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "String < String", CompactNodeTitle = "<", Keywords = "< less"), Category = "Open Unreal Utilities|Operators")
+	static bool Less_StringString(const FString& A, const FString& B);
+
+	/** Returns true if A is lexically greater than B (A > B) */
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "String > String", CompactNodeTitle = ">", Keywords = "> greater"), Category = "Open Unreal Utilities|Operators")
+	static bool Greater_StringString(const FString& A, const FString& B);
+
+	/** Returns true if A is lexically less than or equal to B (A <= B) */
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "String <= String", CompactNodeTitle = "<=", Keywords = "<= less"), Category="Open Unreal Utilities|Operators")
+	static bool LessEqual_StringString(const FString& A, const FString& B);
+
+	/** Returns true if A is lexically greater than or equal to B (A >= B) */
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "String >= String", CompactNodeTitle = ">=", Keywords = ">= greater"), Category="Open Unreal Utilities|Operators")
+	static bool GreaterEqual_StringString(const FString& A, const FString& B);
+
+	// FName
+
+	/** Returns true if A is lexically less than B (A < B) */
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "Name < Name", CompactNodeTitle = "<", Keywords = "< less"), Category = "Open Unreal Utilities|Operators")
+	static bool Less_NameName(const FName& A, const FName& B);
+
+	/** Returns true if A is lexically greater than B (A > B) */
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "Name > Name", CompactNodeTitle = ">", Keywords = "> greater"), Category = "Open Unreal Utilities|Operators")
+	static bool Greater_NameName(const FName& A, const FName& B);
+
+	/** Returns true if A is lexically less than or equal to B (A <= B) */
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "Name <= Name", CompactNodeTitle = "<=", Keywords = "<= less"), Category="Open Unreal Utilities|Operators")
+	static bool LessEqual_NameName(const FName& A, const FName& B);
+
+	/** Returns true if A is lexically greater than or equal to B (A >= B) */
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "Name >= Name", CompactNodeTitle = ">=", Keywords = ">= greater"), Category="Open Unreal Utilities|Operators")
+	static bool GreaterEqual_NameName(const FName& A, const FName& B);
+};
