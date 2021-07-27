38833a1c752fc51a8f7983cd0a889a491f7e23d2
Jonas Reich <mail@jonasreich.de>
2021-07-27 21:49:04 +0200

    Added OUUDataTableLibrary

---
+++ Source/OUUBlueprintRuntime/Private/Core/OUUDataTableLibrary.cpp
@@ -0,0 +1,97 @@
+// Copyright (c) 2021 Jonas Reich
+
+#include "Core/OUUDataTableLibrary.h"
+
+bool UOUUDataTableLibrary::AddRowToDataTable(UDataTable* DataTable, FName RowName, FTableRowBase RowStruct)
+{
+	// We must never hit this! The real implementation is in Generic_AddRowToDataTable 
+	check(false);
+	return false;
+}
+
+bool UOUUDataTableLibrary::RemoveRowFromDataTable(UDataTable* DataTable, FName RowName)
+{
+	if (!DataTable->GetRowNames().Contains(RowName))
+	{
+		return false;
+	}
+	DataTable->RemoveRow(RowName);
+	return true;
+}
+
+DEFINE_FUNCTION(UOUUDataTableLibrary::execAddRowToDataTable)
+{
+	// Steps into the stack, walking to the next properties in it
+	P_GET_OBJECT(UDataTable, DataTable);
+	P_GET_PROPERTY(FNameProperty, RowName);
+
+	Stack.StepCompiledIn<FStructProperty>(nullptr);
+	void* RowPtr = Stack.MostRecentPropertyAddress;
+
+	// We need this to wrap up the stack
+	P_FINISH;
+
+	// Grab the last property found when we walked the stack
+	// This does not contains the property value, only its type information
+	FStructProperty* StructProp = CastField<FStructProperty>(Stack.MostRecentProperty);
+
+	bool bSuccess = false;
+	if (!DataTable)
+	{
+		FBlueprintExceptionInfo ExceptionInfo(
+			EBlueprintExceptionType::AccessViolation,
+			INVTEXT("Failed to resolve the table input. Be sure the DataTable is valid.")
+		);
+		FBlueprintCoreDelegates::ThrowScriptException(P_THIS, Stack, ExceptionInfo);
+	}
+	else if (StructProp && RowPtr)
+	{
+		UScriptStruct* OutputType = StructProp->Struct;
+		const UScriptStruct* TableType  = DataTable->GetRowStruct();
+
+		const bool bCompatible = (OutputType == TableType) || 
+			(OutputType->IsChildOf(TableType) && FStructUtils::TheSameLayout(OutputType, TableType));
+		if (bCompatible)
+		{
+			P_NATIVE_BEGIN;
+			bSuccess = Generic_AddRowToDataTable(DataTable, RowName, StructProp, RowPtr);
+			P_NATIVE_END;
+		}
+		else
+		{
+			FBlueprintExceptionInfo ExceptionInfo(
+				EBlueprintExceptionType::AccessViolation,
+				INVTEXT("Incompatible input parameter; the data table's type is not the same as the type to add.")
+				);
+			FBlueprintCoreDelegates::ThrowScriptException(P_THIS, Stack, ExceptionInfo);
+		}
+	}
+	else
+	{
+		FBlueprintExceptionInfo ExceptionInfo(
+			EBlueprintExceptionType::AccessViolation,
+			INVTEXT("Failed to resolve the input parameter for AddRowToDataTable.")
+		);
+		FBlueprintCoreDelegates::ThrowScriptException(P_THIS, Stack, ExceptionInfo);
+	}
+
+	*(bool*)RESULT_PARAM = bSuccess;
+}
+
+bool UOUUDataTableLibrary::Generic_AddRowToDataTable(UDataTable* DataTable, FName RowName, FStructProperty* StructProp, void* RowPtr)
+{
+	if (!IsValid(DataTable) || !RowPtr || !StructProp)
+		return false;
+
+	if (DataTable->GetRowMap().Contains(RowName))
+		return false;
+	
+	const UScriptStruct* StructType = DataTable->GetRowStruct();
+	if (StructType == nullptr)
+		return false;
+
+	const FTableRowBase* RowAsTableRowBase = StaticCast<FTableRowBase*>(RowPtr);
+
+	DataTable->AddRow(RowName, *RowAsTableRowBase);
+	return true;
+}
---
+++ Source/OUUBlueprintRuntime/Public/Core/OUUDataTableLibrary.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2021 Jonas Reich
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Engine/DataTable.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OUUDataTableLibrary.generated.h"
+
+/**
+ * Extensions to UDataTableFunctionLibrary, i.e. functions to manipulate data tables
+ * from Blueprint that are otherwise available from code. 
+ */
+UCLASS()
+class UOUUDataTableLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	/**
+	 * Adds a row to a data table. Fails if the row name already exists in the table.
+	 * @param DataTable: The data table to add the row to
+	 * @param RowName: Name key of the new row. Must be unique inside the table.
+	 * @param RowStruct: Custom structure to use as values for the new table row
+	 * @returns if the row was successfully added to the data table
+	 */
+	UFUNCTION(BlueprintCallable, CustomThunk, meta = (CustomStructureParam = "RowStruct"), Category = "Open Unreal Utilities|DataTable")
+	static bool AddRowToDataTable(UDataTable* DataTable, FName RowName, FTableRowBase RowStruct);
+
+	/**
+	 * Removes a row from a data table if it exists.
+	 * @param DataTable: The data table to remove the row from
+	 * @param RowName: Name key of the row that should be removed
+	 * @returns if the row was successfully removed from the data table
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Open Unreal Utilities|DataTable")
+	static bool RemoveRowFromDataTable(UDataTable* DataTable, FName RowName);
+
+private:
+	DECLARE_FUNCTION(execAddRowToDataTable);
+
+	static bool Generic_AddRowToDataTable(UDataTable* DataTable, FName RowName, FStructProperty* StructProp, void* RowPtr);
+};
