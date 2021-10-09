ecca3df987892ee5c76f2ce1f8c40dbfa104b307
Jonas Reich <mail@jonasreich.de>
2021-10-09 22:34:31 +0200

    Changed bitmask utility template parameters

--- Source/OUURuntime/Public/Templates/BitmaskUtils.h
+++ Source/OUURuntime/Public/Templates/BitmaskUtils.h
@@ -47,21 +47,26 @@ struct TEnumSequenceTraits
 
 /**
  * Utility functions to deal with bitmask creation and evaluation.
+ * 
  * All of the functions require the following template arguments:
- * - EnumType: The enum class to use as flags
- * - MaskIntegerType: The integer type to use for the mask
- * - SequenceType: How cases are distributed in the enum (see EEnumSequence declaration)
+ * - EnumType:
+ *       The enum class to use as flags
+ * - SequenceType (optional):
+ *       How cases are distributed in the enum (see EEnumSequence declaration).
+ *       This argument is optional: By default it's filled in from TEnumSequenceTraits.
+ * - MaskIntegerType (optional):
+ *       The integer type to use for the mask
+ * 
  */
 namespace BitmaskUtils
 {
-	template <class EnumType, typename MaskIntegerType = int32,
-	          EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr MaskIntegerType EnumValueAsBitmask(EnumType EnumValue)
 	{
 		// This is the only function that really uses the SequenceType.
 		// Because it's used everywhere else it's also sufficient to only have the static asserts in this single function.
 
-		static_assert(TIsEnumClass<EnumType>::Value, "EnumType must be an enum class");
+		static_assert(TIsEnum<EnumType>::Value, "EnumType must be an enum or enum class");
 		static_assert(TIsInteger<MaskIntegerType>::Value, "MaskIntegerType must be an integer type");
 		static_assert(sizeof(MaskIntegerType) >= sizeof(EnumType) || SequenceType == EEnumSequenceType::Natural,
 			"MaskIntegerType must be at least as big as underlying type of EnumType, so there is enough space for all flags");
@@ -69,64 +74,96 @@ namespace BitmaskUtils
 		static_assert(SequenceType != EEnumSequenceType::Undefined, "SequenceType not defined for EnumType. "
 			"You need to declare TEnumSequenceTraits specialization for EnumType so the correct bitmask conversion method can be determined.");
 
-		return SequenceType == EEnumSequenceType::Pow2 ? StaticCast<MaskIntegerType>(EnumValue) : (1 << StaticCast<MaskIntegerType>(EnumValue));
+		const EEnumSequenceType TraitSequenceType = TEnumSequenceTraits<EnumType>::Type;
+		static_assert(TraitSequenceType == EEnumSequenceType::Undefined || TraitSequenceType == SequenceType,
+			"SequenceType is defined via TEnumSequenceTraits but also specified explicitly for BitmaskUtils template and the values mismatch! "
+			"Please do not explicitly set the sequence type when invoking the template functions for enum types that have it defined via trait.");
+
+		// Use static_cast instead of StaticCast, because the latter is not constexpr
+		return SequenceType == EEnumSequenceType::Pow2 ? static_cast<MaskIntegerType>(EnumValue) : (1 << static_cast<MaskIntegerType>(EnumValue));
 	}
 
-	template <class EnumType, typename MaskIntegerType = int32>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr void SetBit(MaskIntegerType& BitmaskValue, EnumType EnumValue)
 	{
-		BitmaskValue |= EnumValueAsBitmask<EnumType, MaskIntegerType>(EnumValue);
+		BitmaskValue |= EnumValueAsBitmask<EnumType, SequenceType, MaskIntegerType>(EnumValue);
 	}
 
-	template <class EnumType, typename MaskIntegerType = int32>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr void SetBits(MaskIntegerType& BitmaskValue, std::initializer_list<EnumType> EnumValues)
 	{
 		for (EnumType Flag : EnumValues)
 		{
-			SetBit<EnumType, MaskIntegerType>(BitmaskValue, Flag);
+			SetBit<EnumType, SequenceType, MaskIntegerType>(BitmaskValue, Flag);
 		}
 	}
 
-	template <class EnumType, typename MaskIntegerType = int32>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr MaskIntegerType EnumValuesAsBitmask(std::initializer_list<EnumType> EnumValues)
 	{
 		MaskIntegerType BitmaskValue = 0;
-		SetBits<EnumType, MaskIntegerType>(BitmaskValue, EnumValues);
+		SetBits<EnumType, SequenceType, MaskIntegerType>(BitmaskValue, EnumValues);
 		return BitmaskValue;
 	}
 
-	template <class EnumType, typename MaskIntegerType = int32>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr void ClearBit(MaskIntegerType& BitmaskValue, EnumType EnumValue)
 	{
-		BitmaskValue &= ~EnumValueAsBitmask<EnumType, MaskIntegerType>(EnumValue);
+		BitmaskValue &= ~EnumValueAsBitmask<EnumType, SequenceType, MaskIntegerType>(EnumValue);
 	}
 
-	template <class EnumType, typename MaskIntegerType = int32>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr void ClearBits(MaskIntegerType& BitmaskValue, std::initializer_list<EnumType> EnumFlags)
 	{
 		for (EnumType Flag : EnumFlags)
 		{
-			BitmaskValue &= ~EnumValueAsBitmask<EnumType, MaskIntegerType>(Flag);
+			BitmaskValue &= ~EnumValueAsBitmask<EnumType, SequenceType, MaskIntegerType>(Flag);
 		}
 	}
 
-	template <class EnumType, typename MaskIntegerType = int32>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
+	constexpr void SetBit(MaskIntegerType& BitmaskValue, EnumType EnumValue, bool bBitValue)
+	{
+		if (bBitValue)
+		{
+			SetBit(BitmaskValue, EnumValue);
+		}
+		else
+		{
+			ClearBit(BitmaskValue, EnumValue);
+		}
+	}
+
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
+	constexpr void SetBits(MaskIntegerType& BitmaskValue, std::initializer_list<EnumType> EnumValues, bool bBitValue)
+	{
+		if (bBitValue)
+		{
+			SetBits(BitmaskValue, EnumValues);
+		}
+		else
+		{
+			ClearBits(BitmaskValue, EnumValues);
+		}
+	}
+
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr bool TestBit(MaskIntegerType BitmaskValue, EnumType EnumValue)
 	{
-		return (BitmaskValue & EnumValueAsBitmask<EnumType, MaskIntegerType>(EnumValue)) > 0;
+		return (BitmaskValue & EnumValueAsBitmask<EnumType, SequenceType, MaskIntegerType>(EnumValue)) > 0;
 	};
 
-	template <class EnumType, typename MaskIntegerType = int32>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr bool TestAllBits(MaskIntegerType BitmaskValue, std::initializer_list<EnumType> EnumFlags)
 	{
-		MaskIntegerType FlagsValue = EnumValuesAsBitmask<EnumType, MaskIntegerType>(EnumFlags);
+		MaskIntegerType FlagsValue = EnumValuesAsBitmask<EnumType, SequenceType, MaskIntegerType>(EnumFlags);
 		return (BitmaskValue & FlagsValue) == FlagsValue;
 	};
 
-	template <class EnumType, typename MaskIntegerType = int32>
+	template<class EnumType, EEnumSequenceType SequenceType = TEnumSequenceTraits<EnumType>::Type, typename MaskIntegerType = int32>
 	constexpr bool TestAnyBits(MaskIntegerType BitmaskValue, std::initializer_list<EnumType> EnumFlags)
 	{
-		MaskIntegerType FlagsValue = EnumValuesAsBitmask<EnumType, MaskIntegerType>(EnumFlags);
+		MaskIntegerType FlagsValue = EnumValuesAsBitmask<EnumType, SequenceType, MaskIntegerType>(EnumFlags);
 		return (BitmaskValue & FlagsValue) > 0;
 	};
-}
+};
