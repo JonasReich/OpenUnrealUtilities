f54c356e384c5224b06c981098c09ebf2626acc9
Jonas Reich <mail@jonasreich.de>
2021-11-08 21:10:47 +0100

    Removed LogTemp from UOUUMathLibrary::SignedAngleBetweenVectors()

--- Source/OUURuntime/Private/Math/OUUMathLibrary.cpp
+++ Source/OUURuntime/Private/Math/OUUMathLibrary.cpp
@@ -11,11 +11,6 @@ float UOUUMathLibrary::AngleBetweenVectors(FVector A, FVector B)
 
 float UOUUMathLibrary::SignedAngleBetweenVectors(FVector A, FVector B, FVector Up)
 {
-	float Angle = AngleBetweenVectors(A, B);
-	UE_LOG(LogTemp, Log, TEXT("A:%s, B:%s, Up:%s, Cross:%s"),
-		*A.ToString(),
-		*B.ToString(),
-		*Up.ToString(),
-		*FVector::CrossProduct(A, B).ToString());
+	const float Angle = AngleBetweenVectors(A, B);
 	return (FVector::DotProduct(Up, FVector::CrossProduct(A, B)) < 0) ? Angle : -Angle;
 }
