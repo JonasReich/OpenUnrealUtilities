f9bb9d574eac46b708e2f26f93a2291519e408f0
Jonas Reich <mail@jonasreich.de>
2021-12-14 21:55:22 +0100

    Added OUUTextLibrary

---
+++ Source/OUURuntime/Private/Localization/OUUTextLocalization.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#include "Localization/OUUTextLibrary.h"
+
+#define LOCTEXT_NAMESPACE "OUUTextLibrary"
+
+FText UOUUTextLibrary::FormatListText(const TArray<FText>& Texts, bool bFormatWithFinalAndSeparator)
+{
+	const int32 TextsNum = Texts.Num();
+	switch (TextsNum)
+	{
+		case 0:
+			return FText();
+		case 1:
+			return Texts[0];
+		default:
+			break;
+	}
+
+	// Combine texts from back to start
+	const int32 SecondToLastIdx = TextsNum - 2;
+	FText CombinedText = bFormatWithFinalAndSeparator ?
+		FormatListText_FinalAndSeparator(Texts[SecondToLastIdx], Texts[SecondToLastIdx + 1]) :
+		FormatListText_GenericSeparator(Texts[SecondToLastIdx], Texts[SecondToLastIdx + 1]);
+
+	for (int32 i = SecondToLastIdx - 1; i >= 0; i--)
+	{
+		CombinedText = FormatListText_GenericSeparator(Texts[i], CombinedText);
+	}
+	return CombinedText;
+}
+
+FText UOUUTextLibrary::FormatListText_GenericSeparator(const FText& TextA, const FText& TextB)
+{
+	return FText::FormatOrdered(LOCTEXT("List.CombineGenericItemsInList", "{0}, {1}"),
+		TextA, TextB);
+}
+
+FText UOUUTextLibrary::FormatListText_FinalAndSeparator(const FText& TextA, const FText& TextB)
+{
+	return FText::FormatOrdered(LOCTEXT("List.CombineFinalItemsInList", "{0} and {1}"),
+		TextA, TextB);
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/OUURuntime/Public/Localization/OUUTextLibrary.h
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2021 Jonas Reich
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OUUTextLibrary.generated.h"
+
+/**
+ * Library for various math utilities not included in FMath or U
+ */
+UCLASS()
+class OUURUNTIME_API UOUUTextLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Format a text that combines a variable number of items into a list text.
+	 * e.g. from an input of n texts, a single text is created:
+	 * - ["foo"] -> "foo"
+	 * - ["foo", "bar"] -> "foo and bar"
+	 * - ["foo", "bar", "foobar"] -> "foo, bar and foobar"
+	 * Supports an arbitrary number of items.
+	 * 
+	 * @param bFormatWithFinalAndSeparator If true, the last two items in the list are combined with a special word ("and" if untranslated).
+	 *     If false, the final two items also use the generic separator ("," if untranslated).
+	 */
+	UFUNCTION(BlueprintPure, Category = "Open Unreal Utilities|Text")
+	static FText FormatListText(const TArray<FText>& Texts, bool bFormatWithFinalAndSeparator);
+
+	/** Combined two texts with a generic separator ("," if untranslated). */
+	UFUNCTION(BlueprintPure, Category = "Open Unreal Utilities|Text")
+	static FText FormatListText_GenericSeparator(const FText& TextA, const FText& TextB);
+
+	/** Combined two texts with a "final" separator ("and" if untranslated). */
+	UFUNCTION(BlueprintPure, Category = "Open Unreal Utilities|Text")
+	static FText FormatListText_FinalAndSeparator(const FText& TextA, const FText& TextB);
+};
