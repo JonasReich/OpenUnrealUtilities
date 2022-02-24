a4abe8b3463d386c7abb3cb5edb130a1a3ef5b51
Jonas Reich <mail@jonasreich.de>
2022-02-24 18:45:49 +0100

    Added FOUURuntimeModule to register gameplay debugger categories from plugin startup

--- Source/OUURuntime/Private/OUURuntimeModule.cpp
+++ Source/OUURuntime/Private/OUURuntimeModule.cpp
@@ -1,5 +1,29 @@
 // Copyright (c) 2022 Jonas Reich
 
 #include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
 
-IMPLEMENT_MODULE(FDefaultModuleImpl, OUURuntime)
+#if WITH_GAMEPLAY_DEBUGGER
+	#include "GameplayAbilities/Debug/GameplayDebuggerCategory_OUUAbilities.h"
+	#include "GameplayDebugger/GameplayDebuggerCategoryTypeList.h"
+	#include "SequentialFrameScheduler/Debug/GameplayDebuggerCategory_SequentialFrameScheduler.h"
+
+using OUU_GameplayDebuggerCategories = TGameplayDebuggerCategoryTypeList<
+	FGameplayDebuggerCategory_OUUAbilities,
+	FGameplayDebuggerCategory_SequentialFrameScheduler>;
+#endif
+
+class FOUURuntimeModule : public IModuleInterface
+{
+#if WITH_GAMEPLAY_DEBUGGER
+	// - IModuleInterface
+	virtual void StartupModule() override
+	{
+		OUU_GameplayDebuggerCategories::RegisterCategories<EGameplayDebuggerCategoryState::Disabled>();
+	}
+	virtual void ShutdownModule() override { OUU_GameplayDebuggerCategories::UnregisterCategories(); }
+	// --
+#endif
+};
+
+IMPLEMENT_MODULE(FOUURuntimeModule, OUURuntime)
