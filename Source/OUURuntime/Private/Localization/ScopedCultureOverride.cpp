fa0cd2918b602c2edf22d2aed32e4fbde508d739
Jonas Reich <mail@jonasreich.de>
2022-03-24 00:25:15 +0100

    Added scoped culture override

---
+++ Source/OUURuntime/Private/Localization/ScopedCultureOverride.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2022 Jonas Reich
+
+#include "Localization/ScopedCultureOverride.h"
+
+#include "LocalizationModule.h"
+
+FScopedCultureOverride::FScopedCultureOverride(const FString& OverrideCultureCode)
+{
+	auto& Internationalization = FInternationalization::Get();
+	Internationalization.BackupCultureState(OriginalCultureState);
+	Internationalization.SetCurrentCulture(OverrideCultureCode);
+}
+
+FScopedCultureOverride::~FScopedCultureOverride()
+{
+	auto& Internationalization = FInternationalization::Get();
+	Internationalization.RestoreCultureState(OriginalCultureState);
+}
---
+++ Source/OUURuntime/Public/Localization/ScopedCultureOverride.h
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2022 Jonas Reich
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Internationalization/Internationalization.h"
+
+struct OUURUNTIME_API FScopedCultureOverride
+{
+public:
+	FScopedCultureOverride(const FString& OverrideCultureCode);
+	~FScopedCultureOverride();
+
+private:
+	FInternationalization::FCultureStateSnapshot OriginalCultureState;
+};
