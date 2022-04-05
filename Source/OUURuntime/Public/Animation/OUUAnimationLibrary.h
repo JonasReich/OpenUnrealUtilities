22552c3896f102cc8d03130eb716f09812e87526
Jonas Reich <mail@jonasreich.de>
2022-04-05 23:00:32 +0200

    Added animation library

---
+++ Source/OUURuntime/Public/Animation/OUUAnimationLibrary.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2022 Jonas Reich
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Animation/AnimInstance.h"
+#include "Animation/AnimLayerInterface.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "OUUAnimationLibrary.generated.h"
+
+UCLASS()
+class OUURUNTIME_API UOUUAnimationLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintPure, Category = "Open Unreal Utilities|Animation")
+	static bool ImplementsAnimationLayerInterface(
+		TSubclassOf<UAnimInstance> AnimInstanceClass,
+		TSubclassOf<UAnimLayerInterface> AnimationLayerInterfaceClass);
+
+	UFUNCTION(BlueprintPure, Category = "Open Unreal Utilities|Animation")
+	static USkeleton* GetAnimInstanceClassTargetSkeleton(TSubclassOf<UAnimInstance> AnimInstanceClass);
+
+	UFUNCTION(BlueprintPure, Category = "Open Unreal Utilities|Animation")
+	static USkeleton* GetAnimInstanceTargetSkeleton(UAnimInstance* AnimInstance);
+};
