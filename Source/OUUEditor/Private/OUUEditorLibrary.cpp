6dad2764cb76e2a4de16d23c5351a4bcb8f7ff39
Jonas Reich <mail@jonasreich.de>
2022-04-22 13:41:48 +0200

    Added generic editor library

---
+++ Source/OUUEditor/Private/OUUEditorLibrary.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2022 Jonas Reich
+
+#include "OUUEditorLibrary.h"
+
+#include "ISessionFrontendModule.h"
+#include "Modules/ModuleManager.h"
+
+void UOUUEditorLibrary::InvokeSessionFrontend(FName Panel)
+{
+	ISessionFrontendModule& SessionFrontend =
+		FModuleManager::LoadModuleChecked<ISessionFrontendModule>("SessionFrontend");
+	SessionFrontend.InvokeSessionFrontend(Panel);
+}
---
+++ Source/OUUEditor/Public/OUUEditorLibrary.h
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2022 Jonas Reich
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "OUUEditorLibrary.generated.h"
+
+UCLASS()
+class OUUEDITOR_API UOUUEditorLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "Open Unreal Utilities|Editor")
+	static void InvokeSessionFrontend(FName Panel = "AutomationPanel");
+};
