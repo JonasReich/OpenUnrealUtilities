b0ce98eef904d9d8afcfeebfd04e86f3d3097576
Jonas Reich <mail@jonasreich.de>
2022-05-30 16:50:17 +0200

    Fixed animaiton editor library compile errors related to changed skeleton API

--- Source/OUUEditor/OUUEditor.Build.cs
+++ Source/OUUEditor/OUUEditor.Build.cs
@@ -33,6 +33,7 @@ public class OUUEditor : OUUModuleRules
 			"UMGEditor",
 			"WorkspaceMenuStructure",
 			"PropertyEditor",
+			"SkeletalMeshEditor",
 			"SkeletalMeshUtilitiesCommon",
 			"EditorFramework",
 
