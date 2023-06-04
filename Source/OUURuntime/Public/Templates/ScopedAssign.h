3404f15751d782804fb941e4d87ca5ffe8de8517
Jonas Reich <mail@jonasreich.de>
2023-06-04 15:03:27 +0200

    Deprecated ScopedAssign

--- Source/OUURuntime/Public/Templates/ScopedAssign.h
+++ Source/OUURuntime/Public/Templates/ScopedAssign.h
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2023 Jonas Reich & Contributors
+// Copyright (c) 2023 Jonas Reich & Contributors
 
 #pragma once
 
@@ -7,7 +7,7 @@
  * The original value is restored after this object runs out of scope.
  */
 template <class T>
-struct TScopedAssign
+struct UE_DEPRECATED(5.1, "Use Unreal's built-in TGuardValue template instead of TScopedAssign") TScopedAssign
 {
 private:
 	T& Field;
