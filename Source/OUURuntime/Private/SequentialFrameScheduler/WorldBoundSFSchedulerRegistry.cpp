e488a41c8f06bd8e3743d450bd6fcdd52bb7b342
Grimlore Buildcat <buildcat@grimloregames.com>
2023-12-12 17:49:36 +0100

    Fixed comment
    Integrated Changes:
    
    - 42899
    
    Co-authored-by: Moritz Kretz <mkretz@grimloregames.com>
    
    [git-p4j: depot-path = "//TQ2S/tq2-code/TQ2/Plugins/OpenUnrealUtilities/..."; change = 42935]

--- Source/OUURuntime/Private/SequentialFrameScheduler/WorldBoundSFSchedulerRegistry.cpp
+++ Source/OUURuntime/Private/SequentialFrameScheduler/WorldBoundSFSchedulerRegistry.cpp
@@ -116,8 +116,8 @@ AWorldBoundSFSchedulerRegistry* AWorldBoundSFSchedulerRegistry::GetWorldSingleto
 		return nullptr;
 	}
 
-	// Any world that's not game or PIE, potentially doesn't tick the scheduler and thus tasks won't be worked on
-	// properly. We just don't support scheduling for these worlds.
+	// Any world that's not game or PIE potentially doesn't tick the scheduler and thus tasks won't be properly worked
+	// on. We just don't support scheduling for these worlds.
 	if (World->WorldType != EWorldType::Game && World->WorldType != EWorldType::PIE)
 	{
 		return nullptr;
