1cb9ea14c0ce865eca268c3aeea378f4fdf23fc4
Jonas Reich <jreich@grimloregames.com>
2024-06-30 13:43:44 +0100

    Added game viewport library with function to change split screen layout
    [git-p4j: depot-path = "//TQ2S/tq2-code/TQ2/Plugins/OpenUnrealUtilities/..."; change = 59661]

---
+++ Source/OUURuntime/Private/Camera/OUUGameViewportLibrary.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2023 Jonas Reich & Contributors
+
+#include "Camera/OUUGameViewportLibrary.h"
+
+#include "Engine/Engine.h"
+#include "Engine/GameViewportClient.h"
+
+void UOUUGameViewportLibrary::UpdateSplitscreenInfo(
+	int32 ScreenIndex,
+	int32 PlayerIndex,
+	FVector2D Origin,
+	FVector2D Size)
+{
+	if (UGameViewportClient* GameViewportClient = GEngine->GameViewport)
+	{
+		auto& SplitscreenInfo = GameViewportClient->SplitscreenInfo;
+		if (SplitscreenInfo.IsValidIndex(ScreenIndex))
+		{
+			auto& PlayerData = GameViewportClient->SplitscreenInfo[ScreenIndex].PlayerData;
+			if (PlayerData.IsValidIndex(PlayerIndex))
+			{
+				PlayerData[PlayerIndex].OriginX = Origin.X;
+				PlayerData[PlayerIndex].OriginY = Origin.Y;
+				PlayerData[PlayerIndex].SizeX = Size.X;
+				PlayerData[PlayerIndex].SizeY = Size.Y;
+			}
+		}
+	}
+}
---
+++ Source/OUURuntime/Public/Camera/OUUGameViewportLibrary.h
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2023 Jonas Reich & Contributors
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SceneView.h"
+
+#include "OUUGameViewportLibrary.generated.h"
+
+UCLASS()
+class OUURUNTIME_API UOUUGameViewportLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "Open Unreal Utilities|Game Viewport")
+	static void UpdateSplitscreenInfo(int32 ScreenIndex, int32 PlayerIndex, FVector2D Origin, FVector2D Size);
+};
