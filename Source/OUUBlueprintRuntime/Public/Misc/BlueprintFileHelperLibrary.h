ae1fc3b1b731ccc033c2fbf727a6e86e9d6c3eb8
Jonas Reich <mail@jonasreich.de>
2024-07-18 22:49:23 +0200

    Added credits widget and parser

---
+++ Source/OUUBlueprintRuntime/Public/Misc/BlueprintFileHelperLibrary.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2023 Jonas Reich & Contributors
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/FileHelper.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BlueprintFileHelperLibrary.generated.h"
+
+/** Expose FFileHelper functionality to Blueprint */
+UCLASS()
+class OUUBLUEPRINTRUNTIME_API UOUUBlueprintFileHelperLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure = false)
+	static bool LoadFileToString(const FString& FilePath, FString& Result)
+	{
+		return FFileHelper::LoadFileToString(OUT Result, *FilePath);
+	}
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false)
+	static bool SaveStringToFile(const FString& String, const FString& FilePath)
+	{
+		return FFileHelper::SaveStringToFile(String, *FilePath, FFileHelper::EEncodingOptions::ForceUTF8);
+	}
+};
---
+++ Source/OUURuntime/Public/Credits/OUUCredits.h
@@ -0,0 +1,60 @@
+﻿// Copyright (c) 2024 Jonas Reich & Contributors
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OUUCredits.generated.h"
+
+UENUM(BlueprintType)
+enum class EOUUCreditsSortMode : uint8
+{
+	KeepInputOrder,
+	AlphabeticalByName,
+	Random
+};
+
+USTRUCT(BlueprintType)
+struct FOUUCreditsEntry
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite)
+	FText Name;
+	UPROPERTY(BlueprintReadWrite)
+	FText Title;
+};
+
+USTRUCT(BlueprintType)
+struct FOUUCreditsBlock
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite)
+	FText BlockTitle;
+	UPROPERTY(BlueprintReadWrite)
+	FSlateBrush Image;
+	UPROPERTY(BlueprintReadWrite)
+	FText BlockDescription;
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FOUUCreditsEntry> People;
+
+	void SortPeople(EOUUCreditsSortMode SortMode);
+};
+
+USTRUCT(BlueprintType)
+struct FOUUCredits
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FOUUCreditsBlock> Blocks;
+};
+
+UCLASS()
+class UOUUCreditsLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	static FOUUCredits CreditsFromMarkdownString(const FString& MarkdownString);
+};
