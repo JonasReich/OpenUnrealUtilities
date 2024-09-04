0317ad450243541320488307d1afdead797a70b0
Jonas Reich <jreich@grimloregames.com>
2024-09-04 18:24:51 +0100

    Added utility function to strip all non "simple ascii" characters from a string
    [git-p4j: depot-path = "//TQ2S/tq2-code/TQ2/Plugins/OpenUnrealUtilities/..."; change = 66619]

---
+++ Source/OUURuntime/Private/Misc/OUUPathUtils.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2024 Jonas Reich & Contributors
+
+#include "Misc/OUUPathUtils.h"
+
+namespace OUU::Runtime::PathUtils
+{
+	FString SanitizeAsciiFileName(FStringView FileName)
+	{
+		static constexpr FAsciiSet ValidAsciiSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
+		FString Result;
+		while (true)
+		{
+			FStringView ValidPrefix = FAsciiSet::FindPrefixWith(FileName, ValidAsciiSet);
+			Result.Append(ValidPrefix);
+			FileName.RemovePrefix(ValidPrefix.Len());
+
+			if (FileName.IsEmpty())
+			{
+				break;
+			}
+
+			FileName.RemovePrefix(1);
+		}
+		return Result;
+	}
+} // namespace OUU::Runtime::PathUtils
---
+++ Source/OUURuntime/Public/Misc/OUUPathUtils.h
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2024 Jonas Reich & Contributors
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Misc/AsciiSet.h"
+
+namespace OUU::Runtime::PathUtils
+{
+	/**
+	 * @returns copy of FileName without any characters not a-zA-Z0-9 range or '_' or '-'.
+	 */
+	OUURUNTIME_API FString SanitizeAsciiFileName(FStringView FileName);
+} // namespace OUU::Runtime::PathUtils
