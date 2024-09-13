e56842f423f2af4843c6726d60022face3aad102
Jonas Reich <jreich@grimloregames.com>
2024-09-13 17:11:02 +0100

    Implemented basic game entitlements system
    [git-p4j: depot-path = "//TQ2S/tq2-code/TQ2/Plugins/OpenUnrealUtilities/..."; change = 67654]

---
+++ Source/OUURuntime/Private/GameEntitlements/OUUGameEntitlements.cpp
@@ -0,0 +1,97 @@
+﻿// Copyright (c) 2024 Jonas Reich & Contributors
+
+#include "Gameentitlements/OUUGameEntitlements.h"
+
+OUU_DEFINE_GAMEPLAY_TAGS(FOUUGameEntitlementTags)
+DEFINE_TYPED_GAMEPLAY_TAG(FOUUGameEntitlementModule)
+DEFINE_TYPED_GAMEPLAY_TAG(FOUUGameEntitlementCollection)
+DEFINE_TYPED_GAMEPLAY_TAG(FOUUGameEntitlementVersion)
+
+extern TAutoConsoleVariable<FString> CVar_OverrideEntitlementVersion;
+
+namespace OUU::Runtime::GameEntitlements
+{
+	void UpdateOverrideEntitlementFromCVar()
+	{
+		auto TagName = FOUUGameEntitlementTags::Version::Get().GetName() + TEXT(".")
+			+ CVar_OverrideEntitlementVersion.GetValueOnGameThread();
+
+		const FGameplayTag RawTag = FGameplayTag::RequestGameplayTag(*TagName, false);
+		const auto VersionTag = FOUUGameEntitlementVersion::TryConvert(RawTag);
+
+		// It's okay or even expected to pass an invalid tag here, because empty/invalid tags will reset the override.
+		UOUUGameEntitlementsSubsystem::Get().SetOverrideVersion(VersionTag);
+	}
+} // namespace OUU::Runtime::GameEntitlements
+
+TAutoConsoleVariable<FString> CVar_OverrideEntitlementVersion{
+	TEXT("ouu.Entitlements.OverrideVersion"),
+	TEXT(""),
+	TEXT("Set an override 'game version' for the entitlements. Format: string for version tag excluding the prefix, "
+		 "e.g. 'Foo' for the 'GameEntitlements.Version.Foo' tag."),
+	FConsoleVariableDelegate::CreateLambda(
+		[](IConsoleVariable*) { OUU::Runtime::GameEntitlements::UpdateOverrideEntitlementFromCVar(); })};
+
+const UOUUGameEntitlementSettings& UOUUGameEntitlementSettings::Get()
+{
+	return *GetDefault<UOUUGameEntitlementSettings>();
+}
+
+UOUUGameEntitlementsSubsystem& UOUUGameEntitlementsSubsystem::Get()
+{
+	return *GEngine->GetEngineSubsystem<UOUUGameEntitlementsSubsystem>();
+}
+
+bool UOUUGameEntitlementsSubsystem::IsEntitled(const FOUUGameEntitlementModule& Module) const
+{
+	// Invalid = empty tag should be treated as asking for "no requirements"
+	return Module.IsValid() == false || ActiveEntitlements.HasTag(Module);
+}
+
+bool UOUUGameEntitlementsSubsystem::IsEntitled(const FOUUGameEntitlementModules_Ref& Modules) const
+{
+	// Expected to return true if Modules is empty
+	return ActiveEntitlements.HasAll(Modules);
+}
+
+void UOUUGameEntitlementsSubsystem::SetOverrideVersion(const FOUUGameEntitlementVersion& Version)
+{
+	OverrideVersion = Version;
+	RefreshActiveVersionAndEntitlements();
+}
+
+void UOUUGameEntitlementsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	OUU::Runtime::GameEntitlements::UpdateOverrideEntitlementFromCVar();
+	RefreshActiveVersionAndEntitlements();
+}
+
+void UOUUGameEntitlementsSubsystem::RefreshActiveVersionAndEntitlements()
+{
+	auto& Settings = UOUUGameEntitlementSettings::Get();
+	ActiveVersion = OverrideVersion.IsValid() ? OverrideVersion : Settings.DefaultVersion;
+	ActiveEntitlements.Reset();
+	if (auto* EntitlementsPtr = Settings.EntitlementsPerVersion.Find(ActiveVersion))
+	{
+		ActiveEntitlements = FOUUGameEntitlementModules_Value::CreateChecked(*EntitlementsPtr);
+	}
+
+	// recursively add entitlements from module collections
+	int32 LastEntitlementCount = -1;
+	while (ActiveEntitlements.Num() != LastEntitlementCount)
+	{
+		for (auto Entitlement : ActiveEntitlements)
+		{
+			auto EntitlementAsCollection = FOUUGameEntitlementCollection::TryConvert(Entitlement);
+			if (EntitlementAsCollection.IsValid())
+			{
+				if (auto* EntitlementsPtr = Settings.ModuleCollections.Find(EntitlementAsCollection))
+				{
+					ActiveEntitlements.AppendTags(FOUUGameEntitlementModules_Value::CreateChecked(*EntitlementsPtr));
+				}
+			}
+		}
+		LastEntitlementCount = ActiveEntitlements.Num();
+	}
+}
---
+++ Source/OUURuntime/Public/GameEntitlements/OUUGameEntitlements.h
@@ -0,0 +1,87 @@
+﻿// Copyright (c) 2024 Jonas Reich & Contributors
+
+#pragma once
+
+#include "Engine/DeveloperSettings.h"
+#include "GameplayTags/TypedGameplayTag.h"
+#include "Subsystems/EngineSubsystem.h"
+
+#include "OUUGameEntitlements.generated.h"
+
+OUU_DECLARE_GAMEPLAY_TAGS_START(OUURUNTIME_API, FOUUGameEntitlementTags, "GameEntitlements", "")
+	OUU_GTAG(
+		Module,
+		"Inidividual modules that can be locked/unlocked",
+		ParentTagType::Flags | EFlags::AllowContentChildTags)
+	OUU_GTAG(
+		Collection,
+		"Meta combination of modules that can be controlled at once",
+		ParentTagType::Flags | EFlags::AllowContentChildTags)
+	OUU_GTAG(
+		Version,
+		"A 'version' of the game that has a predefined selection of modules that are available",
+		ParentTagType::Flags | EFlags::AllowContentChildTags)
+OUU_DECLARE_GAMEPLAY_TAGS_END(FOUUGameEntitlementTags)
+
+USTRUCT(BlueprintType)
+struct OUURUNTIME_API FOUUGameEntitlementModule : public FTypedGameplayTag_Base
+{
+	GENERATED_BODY()
+	IMPLEMENT_TYPED_GAMEPLAY_TAG(FOUUGameEntitlementModule, FOUUGameEntitlementTags::Module)
+};
+
+USTRUCT(BlueprintType)
+struct OUURUNTIME_API FOUUGameEntitlementCollection : public FTypedGameplayTag_Base
+{
+	GENERATED_BODY()
+	IMPLEMENT_TYPED_GAMEPLAY_TAG(FOUUGameEntitlementCollection, FOUUGameEntitlementTags::Collection)
+};
+
+USTRUCT(BlueprintType)
+struct OUURUNTIME_API FOUUGameEntitlementVersion : public FTypedGameplayTag_Base
+{
+	GENERATED_BODY()
+	IMPLEMENT_TYPED_GAMEPLAY_TAG(FOUUGameEntitlementVersion, FOUUGameEntitlementTags::Version)
+};
+
+UCLASS(BlueprintType, Config = "Game", DefaultConfig)
+class OUURUNTIME_API UOUUGameEntitlementSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+public:
+	static const UOUUGameEntitlementSettings& Get();
+
+	// Which version to apply if nothing is overridden from command line or console variables.
+	UPROPERTY(Config, EditAnywhere)
+	FOUUGameEntitlementVersion DefaultVersion;
+
+	UPROPERTY(Config, EditAnywhere, meta = (Categories = "TypedTag{OUUGameEntitlementModule}"))
+	TMap<FOUUGameEntitlementVersion, FGameplayTagContainer> EntitlementsPerVersion;
+
+	UPROPERTY(Config, EditAnywhere, meta = (Categories = "TypedTag{OUUGameEntitlementModule}"))
+	TMap<FOUUGameEntitlementCollection, FGameplayTagContainer> ModuleCollections;
+};
+
+// Central subsystem to track entitlements
+UCLASS()
+class OUURUNTIME_API UOUUGameEntitlementsSubsystem : public UEngineSubsystem
+{
+	GENERATED_BODY()
+public:
+	static UOUUGameEntitlementsSubsystem& Get();
+
+	bool IsEntitled(const FOUUGameEntitlementModule& Module) const;
+	bool IsEntitled(const FOUUGameEntitlementModules_Ref& Modules) const;
+
+	void SetOverrideVersion(const FOUUGameEntitlementVersion& Version);
+
+	// - USubsystem
+	void Initialize(FSubsystemCollectionBase& Collection) override;
+
+private:
+	void RefreshActiveVersionAndEntitlements();
+
+	FOUUGameEntitlementVersion OverrideVersion;
+	FOUUGameEntitlementVersion ActiveVersion;
+	FOUUGameEntitlementModules_Value ActiveEntitlements;
+};
