f5a87754b74fa456514639699c8741874fa5b876
David Lehn <dlehn@grimloregames.com>
2025-03-25 12:03:39 +0100

    Added ouu.Slate.ApplicationScale console command to test global application scale multiplier
    Integrated Changes:
    - 84212 by jreich
    
    [git-p4j: depot-path = "//TQ2S/tq2-code/TQ2/Plugins/OpenUnrealUtilities/..."; change = 84768]

---
+++ Source/OUURuntime/Private/Slate/OUUApplicationScaleCommand.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2025 Jonas Reich & Contributors
+
+#include "Engine/UserInterfaceSettings.h"
+
+namespace
+{
+	void SetSlateApplicationScale(const TArray<FString>& Args)
+	{
+		if (Args.Num() > 0)
+		{
+			float Scale = 1.0f;
+			LexFromString(Scale, *Args[0]);
+			GetMutableDefault<UUserInterfaceSettings>()->ApplicationScale = Scale;
+		}
+	}
+
+	FAutoConsoleCommand CCommand_SetSlateApplicationScale{
+		TEXT("ouu.Slate.ApplicationScale"),
+		TEXT("Override the UI settings application scale to quickly test different resolution scales"),
+		FConsoleCommandWithArgsDelegate::CreateStatic(&SetSlateApplicationScale),
+		ECVF_Default};
+} // namespace
