dba1fa5c0b7848930962752f7feb66a46871e870
Jonas Reich <jreich@grimloregames.com>
2025-11-05 17:44:15 +0100

    Added asset validator that checks for restricted folder names in asset path unless explicitly added to the DefaultGame.ini
    [git-p4j: depot-path = "//TQ2S/tq2-code/TQ2/Plugins/OpenUnrealUtilities/..."; change = 109343]

---
+++ Source/OUUEditor/Private/AssetValidation/OUURestrictedNamesValidator.cpp
@@ -0,0 +1,77 @@
+﻿// Copyright (c) 2025 Jonas Reich & Contributors
+
+#include "AssetValidation/OUURestrictedNamesValidator.h"
+
+bool UOUURestrictedNamesValidator::CanValidateAsset_Implementation(
+	const FAssetData& InAssetData,
+	UObject* InObject,
+	FDataValidationContext& InContext) const
+{
+	if (InObject->IsA<UObjectRedirector>())
+	{
+		return false;
+	}
+
+	return true;
+}
+
+EDataValidationResult UOUURestrictedNamesValidator::ValidateLoadedAsset_Implementation(
+	const FAssetData& InAssetData,
+	UObject* InAsset,
+	FDataValidationContext& Context)
+{
+	// Use the package name instead of package path to make sure every folder starts and ends in a slash to simplify
+	// checks below.
+	auto PackagePath = InAssetData.PackageName.ToString();
+	if (PackagePath.StartsWith(TEXT("/Game/")) == false)
+	{
+		// Never validate content outside the main project content dir
+		return EDataValidationResult::Valid;
+	}
+
+	bool IsRestrictedPath = FPaths::IsRestrictedPath(PackagePath);
+	if (IsRestrictedPath == false)
+	{
+		// The C++ restricted path list (in 5.2) doesn't contain all the names that are checked by UAT / packaging code
+		// in C#. It only adds "active" confidential platforms and internal dirs like "NotForLicensees". So we need to
+		// check for some additional restricted keywords (especially ones that easily cause conflicts with props):
+		IsRestrictedPath |= PackagePath.Contains(TEXT("/Apple/"));
+		IsRestrictedPath |= PackagePath.Contains(TEXT("/Desktop/"));
+		IsRestrictedPath |= PackagePath.Contains(TEXT("/Switch/"));
+		IsRestrictedPath |= PackagePath.Contains(TEXT("/Windows/"));
+	}
+
+	if (IsRestrictedPath)
+	{
+		if (FApp::IsEngineInstalled() == false)
+		{
+			// check for allowed directories if the engine is not installed
+			FString LocalPath;
+			FPackageName::TryConvertLongPackageNameToFilename(PackagePath, LocalPath);
+			FPaths::MakePathRelativeTo(LocalPath, *FPaths::RootDir());
+			TArray<FString> AllowedDirectories;
+			GConfig->GetArray(TEXT("Staging"), TEXT("AllowedDirectories"), OUT AllowedDirectories, GGameIni);
+			TArray<FString> AllowedPlatformDirectories;
+			GConfig->GetArray(
+				TEXT("Staging"),
+				TEXT("AllowedPlatformDirectories"),
+				OUT AllowedPlatformDirectories,
+				GGameIni);
+			AllowedDirectories.Append(AllowedPlatformDirectories);
+			for (auto& Dir : AllowedDirectories)
+			{
+				if (LocalPath.StartsWith(Dir))
+				{
+					return EDataValidationResult::Valid;
+				}
+			}
+		}
+
+		Context.AddError(INVTEXT("Asset is in a restricted platform specific directory (e.g. Windows/XSX/Apple/etc). "
+								 "Please rename or add an exception to Game.ini [Staging]AllowedDirectories if this is "
+								 "NOT a platform directory or [Staging]AllowedPlatformDirectories if it IS."));
+		return EDataValidationResult::Invalid;
+	}
+
+	return EDataValidationResult::Valid;
+}
---
+++ Source/OUUEditor/Public/AssetValidation/OUURestrictedNamesValidator.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2025 Jonas Reich & Contributors
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "EditorValidatorBase.h"
+#include "OUUAssetValidatorBase_EngineVersionGlue.h"
+
+#include "OUURestrictedNamesValidator.generated.h"
+
+// Validates all assets to check if they use any restricted names not enforced by the engine.
+UCLASS()
+class UOUURestrictedNamesValidator : public UOUUAssetValidatorBase_EngineVersionGlue
+{
+	GENERATED_BODY()
+public:
+	// - UEditorValidatorBase / UOUUAssetValidatorBase_EngineVersionGlue (depending on engine version)
+	bool CanValidateAsset_Implementation(
+		const FAssetData& InAssetData,
+		UObject* InObject,
+		FDataValidationContext& InContext) const override;
+	EDataValidationResult ValidateLoadedAsset_Implementation(
+		const FAssetData& InAssetData,
+		UObject* InAsset,
+		FDataValidationContext& Context) override;
+	// --
+};
